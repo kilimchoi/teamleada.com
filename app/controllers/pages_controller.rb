1249ba45357dc2fd25e0bc803e8527b79a783680
Mark Miyashita <negativetwelve@gmail.com>
2014-03-16 11:47:37 -0700

    Add r-fiddle and test page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,4 +9,7 @@ class PagesController < ApplicationController
   def handbook
   end
 
+  def test
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ TeachData::Application.routes.draw do
 
   match "about", to: "pages#about", via: :get
   match "handbook", to: "pages#handbook", via: :get
+  match "test", to: "pages#test", via: :get
 
   resources :projects, param: :url, only: [:show]
 end
