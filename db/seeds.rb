16d0840f9c0d5df16fd0f1b9e11e390a43eba2e5
Mark Miyashita <negativetwelve@gmail.com>
2014-03-19 16:41:17 -0700

    Misinterpreted the steps

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,9 @@ puts "Created project: #{project.title}."
 welcome_lesson_content = "Welcome to your first data project! Here you will learn several tools for data analysis, build models, and do exploratory analysis! This project is taken from Kaggle, a data science competition website. The project is calledTitanic: Machine Learning from Disaster.
 
 You are tasked to predict whether a passenger survived the Titanic crash. You are given two datasets (Train & Test) each of which include predictor variables such as Age, Passenger Class, Sex, etc. With these two data sets we will do the following:
+
+1. Create a model which will predict whether a passenger survived using only the Train data set
+2. Predict whether the passengers survived in the Test data set based on the model we created
 "
 
 welcome_lesson = Lesson.create!(
@@ -26,8 +29,8 @@ welcome_lesson = Lesson.create!(
 project.lessons << welcome_lesson
 
 welcome_lesson_steps = [
-  "Create a model which will predict whether a passenger survived using only the Train data set",
-  "Predict whether the passengers survived in the Test data set based on the model we created",
+  "Work with the data",
+  "Learn and apply Analytics tools here",
 ]
 
 welcome_lesson_steps.each do |content|
