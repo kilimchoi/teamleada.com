219a48a83075bc07be42131b6e64ea3f66dc8674
Mark Miyashita <negativetwelve@gmail.com>
2014-03-19 16:37:27 -0700

    Add show methods for lessons and steps

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,2 +1,13 @@
 class LessonsController < ApplicationController
+  before_filter :lesson, only: [:show]
+
+  def show
+  end
+
+  private
+
+  def lesson
+    @lesson = Lesson.find(params[:id])
+  end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 
   def home
+    @project = Project.first
   end
 
   def about
