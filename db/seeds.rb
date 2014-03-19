0e8a57b4e7bbc0946613ccb57987ae0b0afa7d93
Mark Miyashita <negativetwelve@gmail.com>
2014-03-19 16:20:07 -0700

    Add first lesson and steps

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,37 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-project = Project.create(
+project = Project.create!(
   title: "Titanic: Machine Learning From Disaster",
   description: "This is a description of the project"
 )
 
 puts "Created project: #{project.title}."
+
+welcome_lesson_content = "Welcome to your first data project! Here you will learn several tools for data analysis, build models,
+and do exploratory analysis! This project is taken from Kaggle, a data science competition website.
+The project is calledTitanic: Machine Learning from Disaster.
+
+You are tasked to predict whether a passenger survived the Titanic crash.
+You are given two datasets (Train & Test) each of which include predictor variables such as Age,
+Passenger Class, Sex, etc. With these two data sets we will do the following:
+"
+
+welcome_lesson = Lesson.create!(
+  title: "Welcome",
+  content: welcome_lesson_content
+)
+
+project.lessons << welcome_lesson
+
+welcome_lesson_steps = [
+  "Create a model which will predict whether a passenger survived using only the Train data set",
+  "Predict whether the passengers survived in the Test data set based on the model we created",
+]
+
+welcome_lesson_steps.each do |content|
+  new_step = Step.create!(
+    content: content
+  )
+  welcome_lesson.steps << new_step
+end
