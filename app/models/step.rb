98512da39801d0e7e25ab9fc7418f9a93edc0356
Mark Miyashita <negativetwelve@gmail.com>
2014-03-19 17:09:55 -0700

    Add back button to steps and lessons

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,10 +1,10 @@
 class Step < ActiveRecord::Base
+  include Rails.application.routes.url_helpers
+
   belongs_to :lesson
   has_many :next_steps, foreign_key: :previous_step_id, class_name: "Step"
 
   belongs_to :previous_step, class_name: "Step"
-
-  include Rails.application.routes.url_helpers
 
   def main_lesson
     unless lesson.nil?
