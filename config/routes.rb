75dd34b31c8593c555f05683d1ad7ec49cbbb304
Mark Miyashita <negativetwelve@gmail.com>
2014-03-21 15:31:57 -0700

    Remove test and handbook pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ TeamLeada::Application.routes.draw do
   root to: "pages#home"
 
   match "about", to: "pages#about", via: :get
-  match "handbook", to: "pages#handbook", via: :get
-  match "test", to: "pages#test", via: :get
 
   resources :projects, param: :url, only: [:show] do
     resources :lessons, param: :url, only: [:show] do
