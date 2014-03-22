e5122e6a9e2d1641b2e2b2fed3256091582d1b3d
Mark Miyashita <negativetwelve@gmail.com>
2014-03-22 04:29:08 -0700

    Add points to steps and create step status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322093516) do
+ActiveRecord::Schema.define(version: 20140322112112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20140322093516) do
     t.datetime "updated_at"
     t.string   "title"
     t.string   "url"
+    t.integer  "points"
   end
 
   create_table "submissions", force: true do |t|
