5038eeef49424617a50bfeedfa14851bb20c90af
Mark Miyashita <negativetwelve@gmail.com>
2014-03-22 00:24:51 -0700

    Add devise users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ TeamLeada::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Add mailer
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
