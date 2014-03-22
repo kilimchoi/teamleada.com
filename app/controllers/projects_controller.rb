3a533ff90ce7898e215e7f118d70b3770a12cae2
Mark Miyashita <negativetwelve@gmail.com>
2014-03-22 00:09:12 -0700

    Add index and @projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,24 @@
 class ProjectsController < ApplicationController
   before_filter :project, only: [:show]
+  before_filter :projects, only: [:index]
 
   def show
+  end
+
+  def index
   end
 
   private
 
   def project
     @project = Project.find_by(url: params[:url])
+  end
+
+  def projects
+    @projects = Project.all
+    5.times do
+      @projects.push(Project.new(title: "Coming soon!", url: "coming-soon"))
+    end
   end
 
 end
