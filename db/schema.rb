bc9127d62ed153c8719a06e181c9bade4c213f24
Mark Miyashita <negativetwelve@gmail.com>
2014-03-22 00:08:49 -0700

    Add enabled to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322064006) do
+ActiveRecord::Schema.define(version: 20140322070749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20140322064006) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "url"
+    t.boolean  "enabled"
   end
 
   create_table "steps", force: true do |t|
