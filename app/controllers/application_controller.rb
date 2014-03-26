9a7603150cb9264991e1f8ece56130fd20377c24
tristantao <tao.tristan@gmail.com>
2014-03-25 23:06:40 -0700

    removed unused code for route in AppCtrller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,25 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
-  unless Rails.application.config.consider_all_requests_local
-  	rescue_from Exception, :with => :render_404
-  	rescue_from ActiveRecord::RecordNotFound, :with => :render_404
-  	rescue_from ActionController::RoutingError, :with => :render_404
-  	rescue_from ActionController::UnknownController, :with => :render_404
-  end
-
-  def render_404
-  	#render :template => '/pages/404.html.slim', :layout => false, :status => :not_found
-  	redirect_to("404") and return
-  	if Rails.application.config.consider_all_requests_local
-  		redirect_to("pages#404") and return
-	  	#render :template => '/pages/404.html.slim', :layout => false, :status => :not_found
-	  end
-	end
-	def render_not_found(exception)
-		log_error(exception)
-		render :template => "/pages/404.html.slim", :status => 404
-	end
-
 end
