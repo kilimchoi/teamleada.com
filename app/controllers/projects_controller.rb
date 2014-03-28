3a0c83c0d5b613ea5946f73aa10a7681c421105f
Mark Miyashita <negativetwelve@gmail.com>
2014-03-28 03:48:19 -0700

    Redirect to the project page on a successful submission

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,13 +13,21 @@ class ProjectsController < ApplicationController
   end
 
   def check_submission
-    score = @project.check_submission
+    score = @project.check_submission(params[:file])
+    @submission = Submission.new(project: @project, user: current_user, score: score)
+    if @submission.save
+      flash[:notice] = "Your score is: #{score}. Check how you did on the LEADAboard!"
+      redirect_to project_path(url: @project.url)
+    else
+      flash[:error] = "There was an error in your submission, please try again."
+      redirect_to :back
+    end
   end
 
   private
 
   def project
-    @project = Project.find_by(url: params[:url])
+    @project = Project.find_by(url: params[:url] || params[:project_url])
   end
 
   def projects
