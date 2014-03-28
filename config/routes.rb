42b48234c8cb20bad4ad583a77d19cdec7835d87
Mark Miyashita <negativetwelve@gmail.com>
2014-03-28 03:47:44 -0700

    Add route for submitting

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ TeamLeada::Application.routes.draw do
   resources :users, param: :username, only: [:show]
 
   resources :projects, param: :url, only: [:show, :index] do
+    match "submit", to: "projects#check_submission", as: "submission", via: :post
+
     resources :lessons, param: :url, only: [:show] do
       resources :steps, param: :url, only: [:show]
     end
