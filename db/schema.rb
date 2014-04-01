0b84d07cc032b114961a6bfeeeeb6c78a549ba56
Mark Miyashita <negativetwelve@gmail.com>
2014-04-01 16:06:40 -0700

    Add slide model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322123221) do
+ActiveRecord::Schema.define(version: 20140401230417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 20140322123221) do
     t.string   "url"
     t.boolean  "enabled"
     t.integer  "number"
+  end
+
+  create_table "slides", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "parent_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "step_requirements", force: true do |t|
