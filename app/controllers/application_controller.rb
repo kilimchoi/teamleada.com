7ac26f12759f1ef37be2135dc59dee97bf5b687b
tristantao <tao.tristan@gmail.com>
2014-04-05 11:43:01 -0700

    redirect after signup/signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,29 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  after_filter :store_location
+
+  def store_location  # store last url - this is needed for post-login redirect to whatever the user last visited.
+  	puts "STORING LOCATION PATH: %s" % request.fullpath
+		puts "STORING CONTENT TYPE: %s" % request.content_type
+		puts "STORING CONTENT FORMATs: %s"  % request.format
+
+  	if (request.fullpath != "/login" &&
+  		request.fullpath != "/logout" &&
+	  		request.fullpath != "/sign_up" &&
+	  		!request.xhr? &&
+	  		(request.format == "text/html" || request.content_type == "text/html"))
+	  	session[:previous_url] = request.fullpath
+	  	session[:last_request_time] = Time.now.utc.to_i
+		end
+		puts "PREV: %s" % session[:previous_url]
+	end
+
+	def after_sign_in_path_for(resource)
+		request.env['omniauth.origin'] || session[:previous_url] || root_path
+	end
+
+	def after_sign_up_path_for(resource)
+		request.env['omniauth.origin'] || session[:previous_url] || root_path
+	end
 end
