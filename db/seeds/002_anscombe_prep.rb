2e6b7bc20baa8840f834418b964692e0f061f72e
tristantao <tao.tristan@gmail.com>
2014-04-15 10:51:27 -0700

    anscombe_intro finished matrx+dataframe create

--- db/seeds/002_anscombe_prep.rb
+++ db/seeds/002_anscombe_prep.rb
@@ -25,7 +25,7 @@ data_object_content = [
 ]
 
 data_object_lesson = Lesson.create!(
-  title: "Different Data Objects",
+  title: "Creating Different Data Objects",
   project: project
 )
 
@@ -45,7 +45,6 @@ vector_data_content_one = [
   ['text', 'what about invalid requests?'],
   ['code', 'vector_one[0]'],
   ['code', 'vector_one[10]'],
-  ['next_steps', nil]
 ]
 vector_data_content_two = [
   ['text', 'Note that you con consider the vector as a matrix vector.'],
@@ -63,7 +62,7 @@ vector_data_content_two = [
 ]
 
 vector_data_step = Step.create!(
-  title: "Data One",
+  title: "Vectors",
   lesson: data_object_lesson
 )
 
@@ -76,6 +75,127 @@ vector_data_slide_one = Slide.create!(
   parent: vector_data_step
 )
 ################################################################################
+
+matrix_data_content_one = [
+	['text' ,'A natural extension of a single vector/vector is the Matrix.'],
+	['code','row_wise = matrix(data=c(3, 4, 5, 6, 7, 8), nrow=2, ncol=3, byrow = TRUE)'],
+	['code','col_wise = matrix(data=c(3, 4, 5, 6, 7, 8), nrow=2, ncol=3, byrow = FALSE)'],
+	['code','row_wise'],
+	['code','col_wise'],
+	['code','class(col_wise)'],
+	['text','Can you guess how this works?'],
+	['text','we feed it the vector data via "data=" option.'],
+	['text','we\'ll tell it how many rows and columns via "nrow=" and "ncol=" option.'],
+	['text','byrow specifies the order in which we populate the matrix row-wise or column-wise.'],
+]
+matrix_data_content_two = [
+	['text', 'what about a mal-formed requests? Try it! We encourage you to try weird things. Who knows, it might work ;)'],
+	['text', 'We try to create a 10 by 10 matrx below, while only giving it 6 data points. What happens?'],
+	['code', 'weird_matrix = matrix(data=c(3, 4, 5, 6, 7, 8), nrow=10, ncol=10, byrow = TRUE)'],
+	['text', 'You get a warning message. But did it work? YES! R tries to create the matrix.'],
+	['text', 'It does so by repeating the data you specified enough times (last repetition is partial) to fill the matrix.'],
+	['text', '(this isn\'t recommended until you\'re more familiar with R)'],
+	['code', 'weird_matrix'],
+	['text', 'Wherelse could you have gotten that information?'],
+	['code', 'help(matrix)'],
+	['text', 'WOAH! It offers the same information we just imparted. It also has useful examples at the very bottom.'],
+	['text', 'help is always there if you need it.'],
+]
+
+matrix_data_step = Step.create!(
+  title: "Matrix",
+  lesson: data_object_lesson
+)
+
+matrix_data_slide_one = Slide.create!(
+  content: matrix_data_content_one,
+  parent: matrix_data_step
+)
+matrix_data_slide_one = Slide.create!(
+  content: matrix_data_content_two,
+  parent: matrix_data_step
+)
+################################################################################
+dataframe_data_content_one = [
+	['text', 'Now that we know a thing or two about matrices, we can move onto dataframes.'],
+	['text', 'Don\'t worry, DataFrames are just like matrices.'],
+	['code', 'my_numbers = c(6, 7, 8)'],
+	['code', 'my_strings = c("I", "Love", "Potatoes")'],
+	['code', 'my_logicals = c(FALSE, FALSE, TRUE)'],
+	['code', 'my_dataframe = data.frame(my_numbers, my_strings, my_logicals)'],
+	['text', 'All we did was append together a few vectors! Check out the dataframe we created.'],
+	['text', 'One thing to note is that the dataframe (can) contain different kinds of data (numbers, words/strings, logicals)'],
+	['code', 'my_dataframe'],
+	['code', 'class(my_dataframe)'],
+]
+
+dataframe_data_content_two = [
+	['code', 'names(my_dataframe)'],
+	['text', 'With the name() function, we were able to acess the column names of the entries.'],
+	['text', 'We can access the columns via the "$" operator. You\'ll get used to this notation soon.'],
+	['code', 'my_dataframe$my_logicals'],
+	['code', 'class(my_dataframe$my_logicals)'],
+	['text', 'You can also access columns as we did before, via column index, starting at 1'],
+	['code', 'my_dataframe[1]'],
+	['code', 'my_dataframe[2]'],
+	['text', 'For now, this isn\'t recommended so we won\'t be using this until you\'re more comfortable with R.'],
+]
+datarame_data_step = Step.create!(
+  title: "Data Frame",
+  lesson: data_object_lesson
+)
+
+datarame_data_slide_one = Slide.create!(
+  content: datarame_data_content_one,
+  parent: datarame_data_step
+)
+datarame_data_slide_one = Slide.create!(
+  content: datarame_data_content_two,
+  parent: datarame_data_step
+)
+################################################################################
+
+
+dataframe_data_content_two = [
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+]
+
+
+
+
+
+
+
+
+
+dataframe_data_content_two = [
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+	['', ''],
+]
+
 
 
 
