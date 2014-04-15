563d5afba5beac6d1402372cf650a20f85f2625f
Mark Miyashita <negativetwelve@gmail.com>
2014-04-15 16:19:13 -0700

    Fix up rakefile and bump submodule

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,7 +1,15 @@
 class BlogController < ApplicationController
 
+  def home
+    render file: File.join(Rails.root, 'app', 'compiled', 'blog', 'index.html')
+  end
+
   def load_stylesheets
-    render file: File.join(Rails.root, 'public', 'blog', "css", "#{params[:path]}.#{params[:format]}")
+    render file: File.join(Rails.root, 'app', 'compiled', 'blog', 'css', "#{params[:path]}.#{params[:format]}")
+  end
+
+  def load_blog_post
+    render file: File.join(Rails.root, 'app', 'compiled', 'blog', "#{params[:path]}")
   end
 
 end
