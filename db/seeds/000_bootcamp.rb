64e54aac77aaa51d22c170f5c7b2a382537b0667
Brian Wayne Liou <brian.liou91@gmail.com>
2014-04-21 11:35:12 -0700

    woops made edits to seed on master

--- db/seeds/000_bootcamp.rb
+++ db/seeds/000_bootcamp.rb
@@ -22,8 +22,8 @@ puts "Created project: #{project.title}."
 
 ##Intro to Basics.
 basic_operations_content_one = [
-  ['text', 'We\'ll first start with some elementary data operations'],
-  ['text', 'This involves multiplying numbers. Remember, R is simply an extremely sophisticated Calculator.'],
+  ['text', 'We\'ll first start with some elementary operations in R.'],
+  ['text', 'In it\'s simplest form, think of R simply as an extremely sophisticated Calculator.'],
   ['lesson_links', nil]
 ]
 
@@ -82,21 +82,21 @@ basic_assignment_content_one = [
   ['next_steps', nil]
 ]
 basic_assignment_content_two = [
-  ['text', 'You can also store assigns words/pharses (or more formally "Character/String") with a double quote.'],
+  ['text', 'You can also store words or phrases, more commonly known as characters or strings in programming, with a double quote.'],
   ['text', 'You can also use single quotes, but we\'ll stick with double for now'],
   ['code', 'greeting = "Hello World!"'],
-  ['code', 'personal_statement = "I hate broccolis!"'],
+  ['code', 'personal_statement = "I hate broccoli!"'],
   ['code', 'favorite_motorcycle = "Ducati"'],
   ['next_steps', nil]
 ]
 basic_assignment_content_three = [
-  ['text', 'At anytime, you can print the numbers by entering it in the Console.'],
+  ['text', 'At anytime, you can return the value of those variables by entering it in the Console.'],
   ['code', 'broccoli'],
   ['code', 'orange'],
   ['code', 'melon'],
   ['code', 'favorite_motorcycle'],
-  ['text', 'You can also explicitly ask R to print by using the print() method.'],
-  ['text', 'Well cover methods/function in more details, but for now trust us and try!'],
+  ['text', 'You can also explicitly ask R to print the value of the variables by using the print() method.'],
+  ['text', 'We\'ll cover methods/function in more details, but for now trust us and try!'],
   ['code', 'print (broccoli)'],
   ['code', 'print (favorite_motorcycle)'],
   ['next_steps', nil]
@@ -119,9 +119,8 @@ basic_assignment_slide_two = Slide.create!(
 )
 ################################################################################
 basic_comparison_content_one = [
-  ['text', 'Now that we have numbers assigned to variables, we can compare the numbers.'],
-  ['text', 'Let\'s see if we can compare some of the variables we assigned above. We can do the followign comparisons:'],
-  ['text', 'Note that what get\'s returned (TRUE or FALSE) are known as Boolean/Logical data types.'],
+  ['text', 'Now that we have integers or character/strings assigned to variables, we can compare them using logical comparisons.'],
+  ['text', 'The possible logical comparisons you can use in R and programming more generally are:'],
   ['text', 'A greater than B : \'A > B\''],
   ['text', 'A grater than or equal to B : \'B >= A\''],
   ['text', 'A smaller than B : \'A < B\''],
@@ -130,14 +129,20 @@ basic_comparison_content_one = [
   ['next_steps', nil]
 ]
 basic_comparison_content_two = [
+  ['text', 'Let\'s see if we can compare some of the variables we assigned above. We can do the following comparisons:'],
+  ['text', 'Note that what R returns, TRUE or FALSE values, are known as Boolean/Logical data types and this is a different data type from integers, characters, or strings'],
   ['text', 'Now let\'s try it out.'],
   ['code', 'melon <= orange'],
   ['code', 'grape == five'],
-  ['code', 'cake > -10'],
-  ['code', 'broccoli < 0.0'],
   ['code', 'favorite_motorcycle == "Yamaha"'],
   ['code', 'favorite_motorcycle == "Ducati"'],
-  ['text', 'Believe it or not, these comparisons shape the backbone of modern programming.'],
+  ['text', 'These comparisons shape the backbone of modern programming.'],
+  ['text', 'The boolean data types in R are TRUE, FALSE, T, or F. Note that if you write it in undercase it errors. Also note that the boolean data type in R defaults to the integer values. FALSE defaults to 0 and TRUE defaults to 1'],
+  ['code', 'true'],
+  ['text', 'That should have returned an error in R'],
+  ['code', 'T + 1'],
+  ['code', 'FALSE == 0'],
+  ['text', 'T + 1 should return 2 and FALSE == 0 should return TRUE'],
   ['next_steps', nil]
 ]
 basic_comparison_step = Step.create!(
@@ -167,12 +172,12 @@ combined_operations_content_one = [
   ['lesson_links', nil]
 ]
 combined_operations_content_two = [
-  ['text', 'What about words/Strings? Do they all work?'],
+  ['text', 'What about characters/strings? Do they all work?'],
   ['code', 'print (personal_statement)'],
   ['code', 'print (personal_statement + " But, " + favorite_motorcycle) #Uh oh, does this work? ;)'],
-  ['text', 'What happened? It helps to read the error (printed in read).'],
+  ['text', 'What happened? It helps to read the error (printed in red).'],
   ['text', 'It seems scary, but if we read the error, it says "non-numeric argument to binary operator".'],
-  ['text', 'R is telling you that you can\'t apply the "+" operator to non-numric (such as string/words) data.'],
+  ['text', 'R is telling you that you can\'t apply the "+" operator to non-numeric (such as characters/strings) data.'],
   ['text', 'How do we get around this? The following will work, but it is just a sneak preview! We\'ll learn more later.'],
   ['code', 'print (paste(personal_statement, favorite_motorcycle, sep=" But, I love "))'],
   ['project_link', 'Back to the lessons page'],
@@ -195,9 +200,9 @@ combined_operations_slide_one = Slide.create!(
 ################################################################################
 ##Intro to DataTypes
 datatypes_intro_content_one = [
-  ['text', 'Now that we know how to do "stuff" in R, let\'s end this tutorial by going over one last concept: Data Types.'],
-  ['text', 'Every time you store a variablbe via an assignment, you\'re storing a specific Data Type.'],
-  ['text', 'Each of these datatypes belong to a class.'],
+  ['text', 'Now that we know how to write basic expressions in R, let\'s end this tutorial by going over one last concept: data types.'],
+  ['text', 'Every time you store a variablbe via an assignment, you\'re storing a specific class.'],
+  ['text', 'R also has five data types: scalars, vectors, matrices, data frames, and lists. We will only go over scalars and vectors in this lesson.'],
   ['lesson_links', nil]
 ]
 datatypes_lesson = Lesson.create!(
@@ -209,51 +214,44 @@ basic_operations_slide_one = Slide.create!(
   parent: datatypes_lesson
 )
 ################################################################################
-classes_content_one = [
-  ['text', 'We saw numbers, strings, true/falses and more. Let\'s inspect by printing the variable and checking its type.'],
+scalars_content_one = [
+  ['text', 'Scalars are the most basic values in R and can be of three classes that you have already been using: integers, characters, and logicals. Verify the classes of objects with the class() function.'],
   ['code', 'orange'],
   ['code', 'class(orange)'],
   ['code', 'favorite_motorcycle'],
   ['code', 'class(favorite_motorcycle)'],
   ['code', 'class(TRUE)'],
   ['code', 'class(cake > broccoli) #remember what the comparison yielded?'],
-  ['text', 'You don\'t have to remember or understand all the datatype; for we simply ask that you remember they exist.'],
-  ['text', 'But what other data object exists?'],
-  ['text', 'We\'ll find out next.'],
   ['project_link', 'Back to the lessons page']
 ]
-classes_content_step = Step.create!(
-  title: "Classes 101",
+scalars_content_step = Step.create!(
+  title: "Scalars 101",
   lesson: datatypes_lesson
 )
-classes_slide_one = Slide.create!(
-  content: classes_content_one,
-  parent: classes_content_step
+scalars_slide_one = Slide.create!(
+  content: scalars_content_one,
+  parent: scalars_content_step
 )
 ################################################################################
 vectors_content_one = [
-  ['text', 'We now have an idea of elemntary data types. What if we want to store multiple data points?'],
-  ['text', 'Set R aside for a moment and try to envision (in your own way) the following sequence "12" "5" "10" "2" "12"'],
-  ['text', 'Think about it... (ans below)'],
-  ['text', 'How did you do it? You probably used a list! This idea is mirrored in R as Vectors'],
-  ['text', 'Vector will be the last idea we go over here. (Note that R also has "lists", but it is a bit different)'],
-  ['text', 'Vector is one of the wyas R stores data. If you\'re comfortable with matrices, you can imagine a vector to be a single column!'],
-  ['text', 'let\'s try it.'],
+  ['text', 'We now have an idea of the most elementary values in R. What if we want to store multiple values?'],
+  ['text', 'We do this in R by creating vectors'],
+  ['text', 'Vectors are the way in which R stores a single dimension of data'],
+  ['text', 'Let\'s try it.'],
   ['next_steps', nil]
 ]
 vectors_content_two = [
-  ['text', 'we\'ll start by creating a simple vector'],
+  ['text', 'we\'ll start by creating a simple vector and using the c() function in R. Think of it as the "concatenate" function.'],
   ['code', 'number_list = c(1,2,3,4)'],
   ['code', 'words_list = c("I", "hate", "vegetables")'],
-  ['text', 'We used the c() function to comine values into a list.'],
   ['text', 'Make sure to have commas, or it will error out. Try and see what error you get!'],
   ['code', 'food_opinion = c("I", "hate" "vegetables")'],
   ['text', 'Can you intepret the message?'],
   ['text', 'It\'s telling us that it was expecting something other than a string(word) constant in the command (i.e. it wants a comma)']
 ]
 vectors_content_three = [
-  ['text', 'Note that if you mix and match, entries will generally get converted into Character/String type.'],
-  ['code', 'Note that quotes around the values. This indicates that they\'ve been converted into Character type.'],
+  ['text', 'Note that if you mix and match classes in R, entries will generally get converted into Character/String type.'],
+  ['code', 'Note that quotes around the values indicates that they\'ve been converted into Character type.'],
   ['code', 'c("I", "once", "ate", 1, "vegetable")'],
   ['code', 'c("there", "is", 1, TRUE, "winner")'],
   ['text', 'Getting familar with error messages will help you become better faster. We promise.'],
@@ -283,9 +281,8 @@ vectors_slide_three = Slide.create!(
 ################################################################################
 ##Intro to DataTypes
 conclusion_content_one = [
-  ['text', 'In this onboarding, we rushed you through some R concepts.'],
-  ['text', 'Though everything may not be clear, you can now do basic arithmetics in R in addition to some data manipulation.'],
-  ['text', 'Believe it or not, you\'ve finished the R bootcamp! The few, the proud, the R-rines! (ar-rines).'],
+  ['text', 'In this onboard to R bootcamp, we taught you how to write basic expressions in R and the basis of data types in the R language.'],
+  ['text', 'You can now do basic arithmetic in R in addition to some data manipulation.'],
   ['text', 'Now, we\'re going to throw you into project specific tutorials. Get ready.'],
   ['project_link', 'Back to the lessons page']
 ]
