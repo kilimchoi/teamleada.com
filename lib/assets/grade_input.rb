b1a16a57e4f9730a5ff90d895f703dd007790405
tristantao <tao.tristan@gmail.com>
2014-04-21 13:01:18 -0700

    fixing ugly tabs

--- lib/assets/grade_input.rb
+++ lib/assets/grade_input.rb
@@ -1,51 +1,51 @@
 
 
 def whitespace_pass(answer, user_input)
-	'''
-	Returns T/F depending of answer == user_input, after we remove all white spaces.
-	'''
-	space_removed_input = user_input.gsub(/\s+/, "")
-	space_removed_answer = answer.gsub(/\s+/, "")
-	if (space_removed_input == space_removed_answer)
-		return true
-	else
-		return false
-	end
+ '''
+ Returns T/F depending of answer == user_input, after we remove all white spaces.
+ '''
+ space_removed_input = user_input.gsub(/\s+/, "")
+ space_removed_answer = answer.gsub(/\s+/, "")
+ if (space_removed_input == space_removed_answer)
+  return true
+ else
+  return false
+ end
 end
 
 def equalsign_pass(answer, user_input)
-	'''
-	Return T/F while taking into account that "<-" and "=" are the same
-	Removes white spaces too
-	'''
-	space_removed_input = user_input.gsub(/\s+/, "")
-	space_removed_answer = answer.gsub(/\s+/, "")
+ '''
+ Return T/F while taking into account that "<-" and "=" are the same
+ Removes white spaces too
+ '''
+ space_removed_input = user_input.gsub(/\s+/, "")
+ space_removed_answer = answer.gsub(/\s+/, "")
 
-	equalized_input = space_removed_input.gsub('<-', '=')
-	equalized_answer = space_removed_answer.gsub('<-', '=')
+ equalized_input = space_removed_input.gsub('<-', '=')
+ equalized_answer = space_removed_answer.gsub('<-', '=')
 
-	if (equalized_input == equalized_answer)
-		return true
-	else
-		return false
-	end
+ if (equalized_input == equalized_answer)
+  return true
+ else
+  return false
+ end
 end
 
 def compare_inputs(answer, user_input)
-	'''
-	Given the correct code "answer" and "user_input", returns True if the user_input
-	renders the same code
-	'''
+ '''
+ Given the correct code "answer" and "user_input", returns True if the user_input
+ renders the same code
+ '''
 
-	if (answer == user_input)
-		return true
-	elsif whitespace_pass(answer, user_input)
-		return true
-	elsif equalsign_pass(answer, user_input)
-		return true
-	else
-		return false
-	end
+ if (answer == user_input)
+  return true
+ elsif whitespace_pass(answer, user_input)
+  return true
+ elsif equalsign_pass(answer, user_input)
+  return true
+ else
+  return false
+ end
 end
 
 puts compare_inputs("my_numbers = c(6, 7, 8)", "my_numbers= c(6, 7,8)")
