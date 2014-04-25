aa7919d3cd4fdf1876ec6df3b4a67a9682c7b0d3
tristantao <tao.tristan@gmail.com>
2014-04-25 14:33:15 -0700

    quiz controller functional

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,14 +1,15 @@
 class QuizzesController < ApplicationController
   before_filter :quiz, only: [:show]
-	before_filter :quizzes, only: [:index]
+  before_filter :quizzes, only: [:index]
+  def check_answer
+    puts "=========== controller.check_answer"
+    @quiz="@quiz in check_answer"
 
- def check_answer
-  puts "=========== controller.check_answer"
-  @quiz="@quiz in check_answer"
-
-  @result = Quiz.find_by_quiz_id(params[:quiz_id])
-  puts @resultanswer
-  puts
+    @result = Quiz.find_by_quiz_id(params[:quiz_id])
+    if @result == nil
+      render :text => "couldn't find quiz id: " + params[:quiz_id]
+      return
+    end
     if @result.answer == params[:user_input] #if answer is same as what user inputed @TODO clean user input.
       render :text => "correct"
     else
