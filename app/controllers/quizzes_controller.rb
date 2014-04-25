4a40b2ce12ea37eb61572e0203b6cc7cebb01f54
tristantao <tao.tristan@gmail.com>
2014-04-25 01:33:43 -0700

    closig end issue

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,12 +1,11 @@
 class QuizzesController < ApplicationController
-	def check_answer
-		@result = Quiz.find_by_quiz_id(params[:quiz_id])
+
+ def check_answer
+  @result = Quiz.find_by_quiz_id(params[:quiz_id])
     if @result == params[:user_input] #if answer is same as what user inputed @TODO clean user input.
       render :text => "correct"
     else
       render :text => @result
     end
-	end
-
-	end
+  end
 end
