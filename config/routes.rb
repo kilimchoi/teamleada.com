fba5e0e23b0148477d57c5049c1743ada72553f2
tristantao <tao.tristan@gmail.com>
2014-04-25 14:12:20 -0700

    added get get member for quiz model

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,11 @@ TeamLeada::Application.routes.draw do
     end
   end
 
-  resources :quizzes
+  resources :quizzes do
+    collection do
+      get :check_answer
+    end
+  end
 
   # 404 page routes
   match "*path", to: "pages#error", via: :get
