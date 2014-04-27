dafe6aed54504503fd5426d89190fd9b62bfe8bd
tristantao <tao.tristan@gmail.com>
2014-04-26 21:04:51 -0700

    bootcamp seed change to integrate one quiz

--- db/seeds/000_bootcamp.rb
+++ db/seeds/000_bootcamp.rb
@@ -41,8 +41,8 @@ basic_operations_slide_one = Slide.create!(
 basic_math_content_one = [
   ['text', 'Try the basic operations.'],
   ['code', '5 + 5'],
+  ['text', 'Enter what you got below:'],
   ['quiz', 'onboard_1'],
-  ['text', 'What did you get? If you got 10, good!'],
   ['text', 'Try a few other to get a feel.'],
   ['code', '1 - 10'],
   ['code', '12 + 178'],
