73bb64fed281ecde3fb9f0c1092ff8ccd468650a
Mark Miyashita <negativetwelve@gmail.com>
2014-05-03 02:01:39 -0700

    Add user code

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140503085755) do
+ActiveRecord::Schema.define(version: 20140503090020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,13 @@ ActiveRecord::Schema.define(version: 20140503085755) do
     t.integer  "user_id"
     t.integer  "project_id"
     t.decimal  "score",      precision: 20, scale: 5
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "user_codes", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "codes_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
