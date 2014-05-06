15d645a2ccab13e4afe401f8cc9baa6755a82d6c
Mark Miyashita <negativetwelve@gmail.com>
2014-05-06 16:50:30 -0700

    Add size and industry options

--- app/models/employer_application.rb
+++ app/models/employer_application.rb
@@ -3,6 +3,11 @@ class EmployerApplication < ActiveRecord::Base
   validates_presence_of :name, :email, :size, :description
   validate :industry_or_other
 
+  INDUSTRY_OPTIONS = ['Agriculture, Forestry, Fishing', 'Mining', 'Construction', 'Manufacturing', 'Transportation',
+                      'Wholesale Trade', 'Retail Trade', 'Finance', 'Services', 'Public Administration', 'Other']
+
+  SIZE_OPTIONS = ['1-5', '5-10', '10-25', '25-50', '50-100', '100-500', '500-1000', '1000+']
+
   private
 
   def industry_or_other
