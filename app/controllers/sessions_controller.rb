36f8e9fd2d541e5f11dec8c7605ea30333e54ba2
Mark Miyashita <negativetwelve@gmail.com>
2014-05-06 21:25:57 -0700

    Fix css on about and main page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < Devise::SessionsController
     session[:return_to] ||= request.referer
 
     self.resource = warden.authenticate!(auth_options)
-    set_flash_message(:notice, :signed_in) if is_flashing_format?
+    set_flash_message(:info, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
     yield resource if block_given?
     respond_with resource, location: after_sign_in_path_for(resource)
