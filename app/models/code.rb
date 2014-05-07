6d9795e67fb645ae89381ae77dcf46fdedd615e6
Mark Miyashita <negativetwelve@gmail.com>
2014-05-06 22:35:14 -0700

    Establish relationships between users and codes

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,4 +1,10 @@
 class Code < ActiveRecord::Base
+  has_many :user_codes
+  has_many :users, through: :user_codes
+
   validates :group, presence: true
   validates :value, uniqueness: true, presence: true
+  validates :user_type, presence: true
+
+  GROUPS = ["project-access"]
 end
