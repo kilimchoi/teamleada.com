4d3bfdc8343763bbd6078d5d31a2428055e8b6af
Mark Miyashita <negativetwelve@gmail.com>
2014-05-08 17:46:43 -0700

    Fix up create charges method

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,23 +6,23 @@ class ChargesController < ApplicationController
   def create
     # Amount in cents
     @amount = 500
+    @transaction = Transaction.find(params[:transaction_id])
 
     customer = Stripe::Customer.create(
-      :email => 'example@stripe.com',
+      :email => current_user.email,
       :card  => params[:stripeToken]
     )
 
     charge = Stripe::Charge.create(
       :customer    => customer.id,
       :amount      => @amount,
-      :description => 'Rails Stripe customer',
+      :description => @transaction.id.to_s,
       :currency    => 'usd'
     )
 
     rescue Stripe::CardError => e
-      flash[:danger] = e.message
-      redirect_to charges_path
-    end
+    flash[:danger] = e.message
+    redirect_to charges_path
   end
 
 end
