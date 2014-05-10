6ad2558f0e0c0b4df85b80864bbcc6dd3fc07239
Mark Miyashita <negativetwelve@gmail.com>
2014-05-09 22:57:56 -0700

    Add users seed file

---
+++ db/seeds/000_users.rb
@@ -0,0 +1,7 @@
+(0..5).each do |index|
+  username = "student#{index}"
+  if User.find_by(username: username).nil?
+    new_user = User.create(username: username, email: "#{username}@#{username}.com", password: "password", role: 'student')
+    puts "Created user: #{new_user.username}."
+  end
+end
