acdae479573c500b011fb93536992070bc3507e3
Brian Wayne Liou <brian.liou91@gmail.com>
2014-05-10 18:17:30 -0700

    edits

---
+++ db/seeds/008_location_rec.rb
@@ -0,0 +1,16 @@
+main_page_content = [
+	['text', 'This is the test project to gauge interest of what kinds of projects our users want.'],
+
+]
+
+project = Project.create!(
+  title:"Create a Location Recommendation System",
+  description: main_page_content,
+  short_description: "Using data from the Bay Area Bike Share you will learn how to create a simple reccomendation algorithm for potential customers. This project will introduce you to Python for data analysis and machine learning. This project is for intermediate level students.",
+  number: 8,
+  enabled: true,
+)
+
+################################################################################
+puts "============ Created project: #{project.title}."
+################################################################################
