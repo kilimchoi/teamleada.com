4cd411aee0182b4d78c4e1630345f39f60a7c9be
Mark Miyashita <negativetwelve@gmail.com>
2014-05-12 13:45:21 -0700

    Move seed files into respective folders

--- db/seeds/000_users.rb
+++
@@ -1,9 +0,0 @@
-if Rails.env.development?
-  (0..5).each do |index|
-    username = "student#{index}"
-    if User.find_by(username: username).nil?
-      new_user = User.create(username: username, email: "#{username}@#{username}.com", password: "password", role: 'student')
-      puts "Created user: #{new_user.username}."
-    end
-  end
-end
--- db/seeds/005_codes.rb
+++
@@ -1,3 +0,0 @@
-if Rails.env.development?
-  Code.create(value: "12345678", group: "project-access", user_type: "test")
-end
