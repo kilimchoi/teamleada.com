5c5b9e9bf412084283985a3c8b8fda41d02f3047
Mark Miyashita <negativetwelve@gmail.com>
2014-05-13 12:58:46 -0700

    Add url to companies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513191033) do
+ActiveRecord::Schema.define(version: 20140513191422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140513191033) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
   end
 
   create_table "company_projects", force: true do |t|
