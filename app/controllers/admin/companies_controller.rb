23a2cd7fa16a40d4cb4fbe68422e60973d5c26e4
Mark Miyashita <negativetwelve@gmail.com>
2014-05-13 12:59:15 -0700

    Add ability to add projects to companies

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -18,6 +18,7 @@ class Admin::CompaniesController < Admin::BaseController
   end
 
   def show
+    @projects = Project.all
   end
 
   def update
@@ -27,6 +28,19 @@ class Admin::CompaniesController < Admin::BaseController
   end
 
   def index
+  end
+
+  def add_project
+    @project = Project.find(params[:company_project][:project_id])
+    @company_project = CompanyProject.find_by(company: @company, project: @project)
+    unless @company_project.nil?
+      flash[:danger] = "You have already assigned #{@project.title} to the comapny #{@company.name}."
+      redirect_to admin_company_path(@company)
+    else
+      @company_project = CompanyProject.create(company: @company, project: @project)
+      flash[:info] = "Success! You have added #{@project.title} to the company #{@company.name}."
+      redirect_to admin_company_path(@company)
+    end
   end
 
   private
