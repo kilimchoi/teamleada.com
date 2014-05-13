d7c2739de0b8c9c37bdb7f48c306efa038581477
Mark Miyashita <negativetwelve@gmail.com>
2014-05-13 14:05:37 -0700

    Add basic confirmation views

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :submissions
@@ -70,6 +70,13 @@ class User < ActiveRecord::Base
       end
     end
     total
+  end
+
+  def password_match?
+    self.errors[:password] << "can't be blank" if password.blank?
+    self.errors[:password_confirmation] << "can't be blank" if password_confirmation.blank?
+    self.errors[:password_confirmation] << "does not match password" if password != password_confirmation
+    password == password_confirmation && !password.blank?
   end
 
 end
