eeb770ffa6e9cf21c9f3ef52744beda880be0f49
Mark Miyashita <negativetwelve@gmail.com>
2014-05-13 18:54:09 -0700

    More mail delivery settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,6 @@ TeamLeada::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -30,7 +27,6 @@ TeamLeada::Application.configure do
   # Add mailer
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
@@ -40,4 +36,8 @@ TeamLeada::Application.configure do
     authentication:       'plain',
     enable_starttls_auto: true
   }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
 end
