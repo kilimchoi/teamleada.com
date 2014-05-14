f14fbeeab5b56a783b0249f978c502f82d7a1549
Mark Miyashita <negativetwelve@gmail.com>
2014-05-13 17:56:12 -0700

    Fix bugs in registration links and add routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ TeamLeada::Application.routes.draw do
                      }
 
   devise_scope :user do
-    match 'confirm', to: "confirmations#confirm", via: :patch
+    match 'confirm', to: "confirmations#confirm", as: :confirmation, via: :patch
   end
 
   resources :users, only: [:show]
