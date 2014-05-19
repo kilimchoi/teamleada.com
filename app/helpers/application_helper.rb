2004162eff7d4965f66b2515d7e66a843252cf81
Mark Miyashita <negativetwelve@gmail.com>
2014-05-18 18:36:26 -0700

    Add sidebar links back to admin dashboard/sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,6 +19,11 @@ module ApplicationHelper
     link_to(name, options, html_options)
   end
 
+  def active_admin_link(name=nil, options={}, html_options={}, &block)
+    html_options[:class] = "#{html_options[:class]} active" if current_page?(options)
+    link_to(name, options, html_options)
+  end
+
   def valid_settings_tab?(tab)
     User::SETTINGS_TABS.include? tab
   end
