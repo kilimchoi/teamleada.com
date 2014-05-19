eaf2cc4ecdfe85ff3cf1182d27d873c8b0113f90
Mark Miyashita <negativetwelve@gmail.com>
2014-05-18 19:00:45 -0700

    Show number of users signed up over time. Fix #232

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,19 +1,21 @@
 class Admin::PagesController < Admin::BaseController
 
   def dashboard
-    @chart = LazyHighCharts::HighChart.new('graph') do |f|
-      f.title(:text => "Population vs GDP For 5 Big Countries [2009]")
-      f.xAxis(:categories => ["United States", "Japan", "China", "Germany", "France"])
-      f.series(:name => "GDP in Billions", :yAxis => 0, :data => [14119, 5068, 4985, 3339, 2656])
-      f.series(:name => "Population in Millions", :yAxis => 1, :data => [310, 127, 1340, 81, 65])
+    @users_data = User.all.group_by{ |user| user.created_at.to_date }
+    @categories = @users_data.keys
+    sum = 0
+    @values = @users_data.values.map{ |array| sum += array.count }
+    @users_chart = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(:text => "Sign ups on Leada over Time")
+      f.xAxis(:categories => @categories)
+      f.series(:name => "Total number of sign ups", :yAxis => 0, :data => @values)
 
       f.yAxis [
-        {:title => {:text => "GDP in Billions", :margin => 70} },
-        {:title => {:text => "Population in Millions"}, :opposite => true},
+        {:title => {:text => "Total number of sign ups", :margin => 70} },
       ]
 
       f.legend(:align => 'right', :verticalAlign => 'top', :y => 75, :x => -50, :layout => 'vertical',)
-      f.chart({:defaultSeriesType=>"column"})
+      f.chart({:defaultSeriesType=>"line"})
     end
   end
 
