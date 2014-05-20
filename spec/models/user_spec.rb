19e1ff7d379ddb4afd6944d90c1bd1bdec4e0a19
Mark Miyashita <negativetwelve@gmail.com>
2014-05-19 21:17:11 -0700

    Annotate user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,27 +2,34 @@
 #
 # Table name: users
 #
-#  id                     :integer          not null, primary key
-#  email                  :string(255)      default(""), not null
-#  encrypted_password     :string(255)      default(""), not null
-#  reset_password_token   :string(255)
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  sign_in_count          :integer          default(0), not null
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :string(255)
-#  last_sign_in_ip        :string(255)
-#  created_at             :datetime
-#  updated_at             :datetime
-#  username               :string(255)
-#  role                   :string(255)
-#  confirmation_token     :string(255)
-#  confirmed_at           :datetime
-#  confirmation_sent_at   :datetime
-#  company_id             :integer
-#  first_name             :string(255)
-#  last_name              :string(255)
+#  id                           :integer          not null, primary key
+#  email                        :string(255)      default(""), not null
+#  encrypted_password           :string(255)      default(""), not null
+#  reset_password_token         :string(255)
+#  reset_password_sent_at       :datetime
+#  remember_created_at          :datetime
+#  sign_in_count                :integer          default(0), not null
+#  current_sign_in_at           :datetime
+#  last_sign_in_at              :datetime
+#  current_sign_in_ip           :string(255)
+#  last_sign_in_ip              :string(255)
+#  created_at                   :datetime
+#  updated_at                   :datetime
+#  username                     :string(255)
+#  role                         :string(255)
+#  confirmation_token           :string(255)
+#  confirmed_at                 :datetime
+#  confirmation_sent_at         :datetime
+#  company_id                   :integer
+#  first_name                   :string(255)
+#  last_name                    :string(255)
+#  unconfirmed_email            :string(255)
+#  updated_password_at          :datetime
+#  who_can_see_profile          :string(255)
+#  who_can_send_friend_requests :string(255)
+#  who_can_contact              :string(255)
+#  who_can_lookup_using_email   :string(255)
+#  who_can_lookup_by_name       :string(255)
 #
 
 require 'spec_helper'
