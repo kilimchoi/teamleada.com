6be2870aee55ae9fb4a003c2ef33d4600233cdf1
Mark Miyashita <negativetwelve@gmail.com>
2014-05-19 21:42:05 -0700

    Actually confirm users

--- db/seeds/development/000_users.rb
+++ db/seeds/development/000_users.rb
@@ -9,22 +9,18 @@ users.each do |user|
   email = "#{user}@#{user}.com"
   if User.find_by(email: email).nil?
     new_user = User.create(username: user, email: email, password: "password", role: 'admin')
-    if new_user.new_record?
-      new_user.confirm!
-    end
+    new_user.confirm!
     puts "Created user: #{new_user.username}."
   end
 end
 
 # Create students
-(0..10).each do |index|
+(0..30).each do |index|
   username = "student#{index}"
   email = "#{username}@#{username}.com"
   if User.find_by(email: email).nil?
     new_user = User.create(username: username, email: email, password: "password", role: 'student')
-    if new_user.new_record?
-      new_user.confirm!
-    end
+    new_user.confirm!
     puts "Created user: #{new_user.username}."
   end
 end
