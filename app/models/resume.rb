aa6b0f897816a54d1081aa15c26db06b39022423
Mark Miyashita <negativetwelve@gmail.com>
2014-05-19 22:06:56 -0700

    Attach resume file with content type of pdf

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,6 +1,6 @@
 class Resume < ActiveRecord::Base
   belongs_to :user
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  has_attached_file :resume_file, styles: { preview: "600x600#", thumb: "100x100#" }
+  validates_attachment_content_type :resume_file, content_type: "application/pdf"
 end
