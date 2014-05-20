31d79da5f9ecc0f120086a776308ab2fb7299268
Mark Miyashita <negativetwelve@gmail.com>
2014-05-19 21:35:38 -0700

    Find users by email. Fix #266

--- db/seeds/development/000_users.rb
+++ db/seeds/development/000_users.rb
@@ -6,8 +6,12 @@ users = [
 
 # Create admins
 users.each do |user|
-  if User.find_by(username: user).nil?
-    new_user = User.create(username: user, email: "#{user}@#{user}.com", password: "password", role: 'admin')
+  email = "#{user}@#{user}.com"
+  if User.find_by(email: email).nil?
+    new_user = User.create(username: user, email: email, password: "password", role: 'admin')
+    if new_user.new_record?
+      new_user.confirm!
+    end
     puts "Created user: #{new_user.username}."
   end
 end
@@ -15,8 +19,12 @@ end
 # Create students
 (0..10).each do |index|
   username = "student#{index}"
-  if User.find_by(username: username).nil?
-    new_user = User.create(username: username, email: "#{username}@#{username}.com", password: "password", role: 'student')
+  email = "#{username}@#{username}.com"
+  if User.find_by(email: email).nil?
+    new_user = User.create(username: username, email: email, password: "password", role: 'student')
+    if new_user.new_record?
+      new_user.confirm!
+    end
     puts "Created user: #{new_user.username}."
   end
 end
