2096fc2d67990922cc454d9b17337d10b1e03a8e
Mark Miyashita <negativetwelve@gmail.com>
2014-05-23 10:09:13 -0700

    Include chart helpers and load a projects chart

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,7 +1,9 @@
 class Admin::ProjectsController < Admin::BaseController
   load_and_authorize_resource
+  include ChartsHelper
 
   def index
+    @projects_chart = multiple_projects_chart(30.days.ago)
   end
 
   def show
