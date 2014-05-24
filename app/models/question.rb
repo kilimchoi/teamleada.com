1e4cd301e95721eb843ed0bce11f581dba1fc0e7
Mark Miyashita <negativetwelve@gmail.com>
2014-05-23 20:29:55 -0700

    Let questions have an asker

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,3 @@
 class Question < ActiveRecord::Base
+  belongs_to :asker, class_name: User
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140524032630) do
+ActiveRecord::Schema.define(version: 20140524032734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 20140524032630) do
     t.integer  "up_votes",    default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "asker_id"
   end
 
   create_table "quizzes", force: true do |t|
