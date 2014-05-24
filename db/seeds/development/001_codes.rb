07c170bc71c6f97d427c72ac6985ff6ad85ca37c
Mark Miyashita <negativetwelve@gmail.com>
2014-05-23 20:00:03 -0700

    Fix codes seed file

--- db/seeds/development/001_codes.rb
+++ db/seeds/development/001_codes.rb
@@ -1,3 +1,3 @@
 if Rails.env.development?
-  Code.create(value: "12345678", group: "project-access", user_type: "test")
+  Code.create(value: "12345678", access_type: "project-access", user_type: "test")
 end
