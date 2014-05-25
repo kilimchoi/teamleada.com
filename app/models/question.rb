e7f2425b0e7a834bf18e07579a7b275419893ed3
Mark Miyashita <negativetwelve@gmail.com>
2014-05-25 09:26:10 -0700

    Link to the up vote button

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,9 @@ class Question < ActiveRecord::Base
   belongs_to :asker, class_name: User
 
   default_scope -> { order(:up_votes) }
+
+  def pretty_post_date
+    created_at.strftime("%B %d, %Y")
+  end
+
 end
