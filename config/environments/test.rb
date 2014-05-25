42c5ec58e43f2de68079b1c7f3869388f2475a2e
Mark Miyashita <negativetwelve@gmail.com>
2014-05-25 08:46:45 -0700

    Set host in test and production

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,4 +35,8 @@ TeamLeada::Application.configure do
   config.active_support.deprecation = :stderr
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.after_initialize do
+    Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+  end
 end
