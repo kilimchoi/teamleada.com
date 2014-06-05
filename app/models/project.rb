b7860f15d4207f1813441138bc08ef102b9c0fb4
Mark Miyashita <negativetwelve@gmail.com>
2014-06-04 19:15:31 -0700

    Fix logic error for computing nonzero points

--- app/models/project.rb
+++ app/models/project.rb
@@ -86,7 +86,7 @@ class Project < ActiveRecord::Base
   def total_points
     total = 0
     lessons.each do |lesson|
-      total += lesson.points || 1
+      total += lesson.points == 0 ? 1 : lesson.points
       lesson.steps.each do |step|
         total += step.points || 1
       end
