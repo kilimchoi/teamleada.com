f3d34be16256b330f6fefbddfc9aed96e80bb355
Mark Miyashita <negativetwelve@gmail.com>
2014-06-04 19:15:57 -0700

    Add todos for changing way we give points later

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,6 +3,9 @@ class LessonsController < ApplicationController
   load_and_authorize_resource :lesson, through: :project
 
   def show
+    # TODO: Right now we're giving points to users when they 'start' a lesson
+    # We should give them the points when they complete the lesson.
+    current_user.complete_lesson(@lesson)
   end
 
 end
--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -4,6 +4,8 @@ class StepsController < ApplicationController
   load_and_authorize_resource :step, find_by: :url
 
   def show
+    # TODO: Right now we're giving users points when they 'start' a step.
+    # Should give them points when they complete it.
     current_user.complete_step(@step)
   end
 
