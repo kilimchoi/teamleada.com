08a18f83ba886db60682843d9f1b554ece359b9d
Mark Miyashita <negativetwelve@gmail.com>
2014-06-06 01:26:11 -0700

    Fix seedfile for complete project button

--- db/seeds/projects/000_introduction_to_r.rb
+++ db/seeds/projects/000_introduction_to_r.rb
@@ -342,8 +342,8 @@ conclusion_content_one = [
   ['text', 'In this R bootcamp, we taught you how to write basic expressions in R as well as the basics of data objects in the R language.'],
   ['text', 'You can now do basic arithmetics in R, in addition to some basic data manipulation.'],
   ['text', 'Use the skills and knowledge gained from here to start completing data projects.'],
-  ['feedback', 'http://www.surveygizmo.com/s3/1654603/Project-Feedback-Form'],
-  ['project_link', 'Back to the Project main page']
+  #['feedback', 'http://www.surveygizmo.com/s3/1654603/Project-Feedback-Form'],
+  ['finish_project_button', 'http://www.surveygizmo.com/s3/1654603/Project-Feedback-Form'],
 ]
 conclusion_lesson = Lesson.create!(
   title: "R Intro Conclusion",
