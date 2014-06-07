c8582e918b3aefe590d5e0a0ebd1a48a22ba45bb
tristantao <tao.tristan@gmail.com>
2014-06-06 17:31:36 -0700

    new titanic starting viz

--- db/seeds/projects/011_titanic_v2.rb
+++ db/seeds/projects/011_titanic_v2.rb
@@ -29,6 +29,7 @@ new_tatanic_intro_content_one = [
   ['text', 'This project is based off of the historic sinking of the Titanic; you are asked to build a model which predicts which passengers survived!'],
   ['text', 'We\'ll be guiding you through a series of analysis stages.'],
   ['text', 'As you complete the steps, paste in your code and responses!'],
+  ['text', 'Next, we\'ll get you setup with downloading the data.'],
 ]
 
 new_tatanic_intro_content_two = [
@@ -49,11 +50,16 @@ welcome_lesson = Lesson.create!(
 )
 
 welcome_lesson_slide_one = Slide.create!(
-  content: welcome_lesson_slide_one_content,
+  content: new_tatanic_intro_content_one,
   parent: welcome_lesson,
   slide_id: 0,
 )
 
+welcome_lesson_slide_two = Slide.create!(
+  content: new_tatanic_intro_content_two,
+  parent: welcome_lesson,
+  slide_id: 1,
+)
 ################################################################################
 
 work_with_data_content = [
@@ -75,19 +81,20 @@ work_with_data_slide_one = Slide.create!(
 )
 
 ################################################################################
-
 train_data_content = [
   ['text', 'To begin working with the data we need to read in the data into R.'],
   ['text', 'Accomplish the following and paste the corresponding code.'],
-  ['text', '- Load the train and test data via read.csv(). Name the raw loaded data "trainData" and "testData"'],
+  ['text', '- Separately load the train and test data via read.csv(). Name the raw loaded data "trainData" and "testData"'],
   ['text', '- Print the first few rows (and/or the last few rows) of the data.'],
+  ['text', 'Paste the code you wrote to accomplish the tasks:'],
   ['user_code', '#@TODO probabl a UID here'],
-  ['text', 'Any Comment on the data?'],
-  ['user_response'. '#@TODO probabl a UID here'],
+  ['text', 'Do you have any thoughts on the data?'],
+  ['user_response', '#@TODO probabl a UID here'],
+  ['next_steps', nil]
 ]
 
 train_data_step = Step.create!(
-  title: "Train Data",
+  title: "Titanic Train Data",
   lesson: work_with_data_lesson,
   step_id: 0,
 )
@@ -99,6 +106,62 @@ train_data_slide_one = Slide.create!(
 )
 
 ################################################################################
+
+train_visualize_content = [
+  ['text', 'Now that we have the data, let\' build some Vizualizations'],
+  ['text', 'Our intuition might be that the women had a higher chance of survival, because the boat adhered to the "Women and Children First" standard.'],
+  ['text', 'What Vizualization would help us check this assumption? Paste your code below: '],
+  ['user_code', '#@TODO probabl a UID here'],
+  ['text', 'What vizualization(s) did you build? Why? What do you see?'],
+  ['user_response', '#@TODO probabl a UID here'],
+  ['next_steps', nil],
+]
+
+train_visualize_step = Step.create!(
+  title: "Visualize Titanic Train Data",
+  previous_step: train_data_step,
+  step_id: 1,
+)
+
+train_visualize_slide_one = Slide.create!(
+  content: train_visualize_content,
+  parent: train_visualize_step,
+  slide_id: 0,
+)
+
+################################################################################
+train_visualize_content = [
+  ['text', 'It is good to first visualize the data to get a general understanding of the patterns and trends of the data. Lets look at the survival rate of our passengers filtered by Sex.'],
+  ['text', 'Your intuition might be that the women had a higher chance of survival because the boat adhered to the "Women and Children First" standard. We first create a table and call it "counts". Then we use the barplot() function in R.'],
+  ['code', 'counts <- table(trainData$Survived, trainData$Sex)'],
+  ['code', 'barplot(counts, xlab = "Gender", ylab = "Number of People", main = "Survival by Sex")'],
+  ['code', 'counts[2] / (counts[1] + counts[2])'],
+  ['code', 'counts[4] / (counts[3] + counts[4])'],
+  ['text', 'The lighter areas indicate survival and notice that our intuition was correct! 74.2% of women survived vs. 18.9% of men. We can use this to improve our model.'],
+  ['next_steps', nil]
+]
+
+train_visualize_step = Step.create!(
+  title: "Visualize Train Data",
+  previous_step: train_data_step,
+  step_id: 1,
+)
+
+train_visualize_slide_one = Slide.create!(
+  content: train_visualize_content,
+  parent: train_visualize_step,
+  slide_id: 0,
+)
+################################################################################
+
+
+
+
+
+
+
+
+
 
 
 
