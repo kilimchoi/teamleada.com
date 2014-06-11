77f00f4c6e4f33240b8fcb7c159f4ed62a63d1f6
Mark Miyashita <negativetwelve@gmail.com>
2014-06-11 02:46:35 -0700

    Add code submission chart

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -7,6 +7,7 @@ class Admin::PagesController < Admin::BaseController
     @interest_chart = interest_chart(30.days.ago)
     @resumes_chart = resumes_chart(30.days.ago)
     @employer_applications_chart = employer_applications_chart(30.days.ago)
+    @code_submission_chart = code_submission_chart(30.days.ago)
   end
 
 end
