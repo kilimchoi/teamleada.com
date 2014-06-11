0fa361933204c6be1934dc0f2bc24e0a016ebf03
Mark Miyashita <negativetwelve@gmail.com>
2014-06-11 00:34:23 -0700

    Add slide index to code submissions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610084117) do
+ActiveRecord::Schema.define(version: 20140611073348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140610084117) do
     t.string   "parent_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "slide_index"
   end
 
   create_table "codes", force: true do |t|
