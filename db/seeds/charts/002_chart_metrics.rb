57c04e8207fbd6ecf0e954d6d6fd4a8dd4da6abd
Mark Miyashita <negativetwelve@gmail.com>
2014-06-12 02:44:12 -0700

    Add chart metric for all project interactions

--- db/seeds/charts/002_chart_metrics.rb
+++ db/seeds/charts/002_chart_metrics.rb
@@ -8,11 +8,15 @@ project_completion_chart = Chart.find(3)
 all_users = Metric.find(0)
 number_of_in_progress_projects = Metric.find(1)
 number_of_completed_projects = Metric.find(2)
+all_project_interactions = Metric.find(3)
 
 # Chart Metrics
 ChartMetric.where(chart: sign_ups_chart, metric: all_users).first_or_create!
+
 ChartMetric.where(chart: users_by_type_chart, metric: all_users).first_or_create!
+
 ChartMetric.where(chart: project_completion_chart, metric: number_of_in_progress_projects).first_or_create!
 ChartMetric.where(chart: project_completion_chart, metric: number_of_completed_projects).first_or_create!
+ChartMetric.where(chart: project_completion_chart, metric: all_project_interactions).first_or_create!
 
 puts "Finished creating ChartMetrics"
