91c75266d23edd20420ed8a419bf76d05fcdee0d
Mark Miyashita <negativetwelve@gmail.com>
2014-06-12 00:51:29 -0700

    Fix seed to use find instead of where

--- db/seeds/charts/002_chart_metrics.rb
+++ db/seeds/charts/002_chart_metrics.rb
@@ -1,12 +1,12 @@
 # Charts
-sign_ups_chart = Chart.where(uid: 0)
-users_by_type_chart = Chart.where(uid: 1)
+sign_ups_chart = Chart.find(0)
+users_by_type_chart = Chart.find(1)
 
 # Metrics
-all_users = Metric.where(uid: 0)
+all_users = Metric.find(0)
 
 # Chart Metrics
-ChartMetric.where(chart_id: sign_ups_chart, metric_id: all_users).first_or_create!
-ChartMetric.where(chart_id: users_by_type_chart, metric_id: all_users).first_or_create!
+ChartMetric.where(chart: sign_ups_chart, metric: all_users).first_or_create!
+ChartMetric.where(chart: users_by_type_chart, metric: all_users).first_or_create!
 
-puts "Finished making ChartMetrics"
+puts "Finished creating ChartMetrics"
