c78f4f17c21e73dca54e9d7d0ed59bfcaf88c50f
Mark Miyashita <negativetwelve@gmail.com>
2014-06-11 23:06:47 -0700

    Link charts and metrics together with chart_metrics

---
+++ app/models/chart_metric.rb
@@ -0,0 +1,2 @@
+class ChartMetric < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140612060343) do
+ActiveRecord::Schema.define(version: 20140612060604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chart_metrics", force: true do |t|
+    t.integer  "chart_id"
+    t.integer  "metric_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "charts", id: false, force: true do |t|
     t.string   "category"
