27b1a32e61e8c57d996ad4d08658a903b6a4f573
Mark Miyashita <negativetwelve@gmail.com>
2014-06-12 02:29:11 -0700

    Made it easier to input metrics

--- db/seeds/charts/001_metrics.rb
+++ db/seeds/charts/001_metrics.rb
@@ -1,12 +1,19 @@
-# Users
-all_users = Metric.where(uid: 0).first_or_create!
-all_users.update(
-  model: "User",
-  title: "All Users",
-  method: "created_before?",
-)
-puts "Created metric: #{all_users.title}"
+metrics = [
+  {
+    uid: 0,
+    model: "User",
+    title: "All Users",
+    method: "created_before?",
+  },
+]
 
 
-# Project Interest
-
+metrics.each do |metric_info|
+  new_metric = Metric.where(uid: metric_info[:uid]).first_or_create!
+  new_metric.update(
+    model: metric_info[:model],
+    title: metric_info[:title],
+    method: metric_info[:method],
+  )
+  puts "Created metric: #{new_metric.title}"
+end
