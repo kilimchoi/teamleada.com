d16a4a69b23dc677ad0ceaf96eb192c2d138ff2b
Mark Miyashita <negativetwelve@gmail.com>
2014-06-12 02:37:06 -0700

    Add method to project status for charts

--- app/models/project_status.rb
+++ app/models/project_status.rb
@@ -1,4 +1,9 @@
 class ProjectStatus < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+
+  # Chart methods
+  def created_before_and_not_completed?(day)
+    created_at <= day.date && !completed?
+  end
 end
