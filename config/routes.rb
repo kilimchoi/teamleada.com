3dc229d59c901f7967b7fa7025358edcbf17a447
Mark Miyashita <negativetwelve@gmail.com>
2014-06-15 22:11:30 -0700

    Add project endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -63,7 +63,11 @@ TeamLeada::Application.routes.draw do
     match '/', to: redirect('/r/dashboard'), via: :get
     match 'dashboard', to: 'pages#dashboard', via: :get
 
-    resources :users, path: "browse", only: [:index, :show]
+    resources :users, path: "browse", only: [:index, :show] do
+      member do
+        match 'projects/:project_id', to: 'users#show_project', via: :get
+      end
+    end
   end
 
   # Admin
