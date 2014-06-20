e38a1e2b146491648d4d51260e4d054dfcd3a754
Mark Miyashita <negativetwelve@gmail.com>
2014-06-19 23:14:30 -0700

    Add viewed_user

--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -1,5 +1,7 @@
 class PageView < ActiveRecord::Base
   belongs_to :user
+  belongs_to :viewed_user, class_name: User,
+                           primary_key: :username
 
   serialize :parameters, Hash
   serialize :properties, Hash
