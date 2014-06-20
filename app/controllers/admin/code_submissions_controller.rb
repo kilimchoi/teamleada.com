b3e64e65efc112b432f83f6a554cbb31463563ba
Mark Miyashita <negativetwelve@gmail.com>
2014-06-20 00:12:31 -0700

    Add evaluate method

--- app/controllers/admin/code_submissions_controller.rb
+++ app/controllers/admin/code_submissions_controller.rb
@@ -5,7 +5,27 @@ class Admin::CodeSubmissionsController < Admin::BaseController
   end
 
   def show
-    @evaluation = CodeSubmissionEvaluation.new(code_submission: @code_submission)
+    @evaluation = CodeSubmissionEvaluation.where(code_submission: @code_submission, reviewer: current_user).first_or_initialize
+  end
+
+  def evaluate
+    @code_submission_evaluation = CodeSubmissionEvaluation.new(evaluation_params)
+    @code_submission_evaluation.reviewee = @code_submission.user
+    @code_submission_evaluation.reviewer = current_user
+    @code_submission_evaluation.code_submission = @code_submission
+    if @code_submission_evaluation.save
+      flash[:info] = "Your evalaution was saved."
+      redirect_to admin_code_submission_path(@code_submission)
+    else
+      flash[:error] = "There was an error creating your evaluation."
+      redirect_to admin_code_submission_path(@code_submission)
+    end
+  end
+
+  private
+
+  def evaluation_params
+    params.require(:code_submission_evaluation).permit(:score, :description)
   end
 
 end
