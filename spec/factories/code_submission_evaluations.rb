29e7c76b3fad8efc37b6d0b8766fce0e4b571e74
Mark Miyashita <negativetwelve@gmail.com>
2014-06-19 23:46:11 -0700

    Correctly annotate model

--- spec/factories/code_submission_evaluations.rb
+++ spec/factories/code_submission_evaluations.rb
@@ -2,13 +2,14 @@
 #
 # Table name: code_submission_evaluations
 #
-#  id          :integer          not null, primary key
-#  reviewer_id :integer
-#  reviewee_id :integer
-#  score       :integer
-#  description :text
-#  created_at  :datetime
-#  updated_at  :datetime
+#  id                 :integer          not null, primary key
+#  reviewer_id        :integer
+#  reviewee_id        :integer
+#  score              :integer
+#  description        :text
+#  created_at         :datetime
+#  updated_at         :datetime
+#  code_submission_id :integer
 #
 
 # Read about factories at https://github.com/thoughtbot/factory_girl
