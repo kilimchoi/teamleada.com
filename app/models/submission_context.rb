2903b6f1b19829acd009dbeb93d44800e25c66ed
tristantao <tao.tristan@gmail.com>
2014-06-21 16:54:48 -0700

    adding more submission type

--- app/models/submission_context.rb
+++ app/models/submission_context.rb
@@ -18,8 +18,12 @@ class SubmissionContext < ActiveRecord::Base
 
   belongs_to :slide
 
-  CODE = "code"
-  RESPONSE = "response"
+  CODE = "code" #code snippets
+  COMPLETE_CODE = "complete_code" #complete src code for a project 
+  RESPONSE = "response" #free response
+  PRES_SLIDES_LINK = "presentation_slides_link" #url to the presentation slides
+  PRES_VIDEO_LINK = "presentation_vid_linK" #url to the presentation video
+
 
   def set_uid
     self.uid = "#{slide.uid}_sc#{submission_context_id}"
