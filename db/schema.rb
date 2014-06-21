05ae3b8bcffce7841249f6ecf1d3ad863f0629e5
tristantao <tao.tristan@gmail.com>
2014-06-21 16:40:34 -0700

    adding submission_type column to submission_context

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140620075614) do
+ActiveRecord::Schema.define(version: 20140621183654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -296,6 +296,7 @@ ActiveRecord::Schema.define(version: 20140620075614) do
     t.datetime "updated_at"
     t.string   "uid",                   null: false
     t.string   "title"
+    t.string   "submission_type"
   end
 
   create_table "submissions", force: true do |t|
