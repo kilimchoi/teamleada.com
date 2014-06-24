c4e89260b2d7eeb1eb6d3729aa190aba40831f93
Mark Miyashita <negativetwelve@gmail.com>
2014-06-23 23:23:42 -0700

    Add invites index page to display invites

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,8 +1,31 @@
-class InvitessController < ApplicationController
+class InvitesController < ApplicationController
   load_and_authorize_resource
 
   def index
     @invite = current_user.invites.build
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.role = "student"
+    if @user.save
+      message = "#{@user.email} has been invited to Leada! Invite #{Invite::INVITES - current_user.invites.count} more friends to receive project access!"
+      if Rails.env.development?
+        secret = @user.generate_new_token
+        message += " DEVELOPMENT MODE: <a href='" + user_confirmation_url(confirmation_token: secret) + "'>Activate</a>"
+      end
+      flash[:info] = message.html_safe
+      redirect_to invites_path
+    else
+      flash[:danger] = "There was an error sending an email to #{@user.email}."
+      redirect_to invites_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:invited_email)
   end
 
 end
