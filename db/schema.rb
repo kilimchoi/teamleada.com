76a26c80331d9b5a08bd67386b5d1f584586dfab
tristantao <tao.tristan@gmail.com>
2014-06-25 15:56:56 -0700

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140625051718) do
+ActiveRecord::Schema.define(version: 20140625061526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -383,8 +383,7 @@ ActiveRecord::Schema.define(version: 20140625051718) do
     t.string   "who_can_lookup_by_name"
     t.string   "who_can_see_resume"
     t.boolean  "looking_for_opportunities",    default: false
-    t.string   "provider"
-    t.string   "uid"
+    t.string   "linkedin_id"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
