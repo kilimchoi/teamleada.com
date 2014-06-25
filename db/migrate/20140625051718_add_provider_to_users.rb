c1bca7a62c49d6ac691ea9b73718428bb4b300df
tristantao <tao.tristan@gmail.com>
2014-06-24 22:55:47 -0700

    changing migration to only add linkedin column: SKETCH, I know

--- db/migrate/20140625051718_add_provider_to_users.rb
+++ db/migrate/20140625051718_add_provider_to_users.rb
@@ -1,6 +1,5 @@
 class AddProviderToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
+    add_column :users, :linkedin_id, :string
   end
 end
