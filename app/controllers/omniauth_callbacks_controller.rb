e70b0166aa807a9a0d67bd18da3746f1db2af146
tristantao <tao.tristan@gmail.com>
2014-06-25 15:58:13 -0700

    moniauth controller, not fully working

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,12 +2,16 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def linkedin
     auth = env["omniauth.auth"]
     @user = User::connect_to_linkedin(request.env["omniauth.auth"],current_user)
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success"
+    if @user.username != nil
+      flash[:notice] = "Success!`"
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.linkedin_uid"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      #session["devise.linkedin_uid"] = request.env["omniauth.auth"]
+      #redirect_to new_user_registration_url
+      debugger
+      byebug
+      redirect_to :linkedin_confirm
+      #redirect_to linkedin_confirm
     end
   end
 end
