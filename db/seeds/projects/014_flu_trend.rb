3962146afc2c43fbe0771eca0014335fd20c0150
tristantao <tao.tristan@gmail.com>
2014-06-27 14:05:44 -0700

    finalizing flu trend data

--- db/seeds/projects/014_flu_trend.rb
+++ db/seeds/projects/014_flu_trend.rb
@@ -7,11 +7,11 @@ main_page_content = [
 project = Project.create!(
   title:"Analyzing Flu Trend",
   description: main_page_content,
-  short_description: "A data project focused on analyzing trend data. We'll be building time series model to make predictions on flu trend.",
+  short_description: "A data project focused on analyzing flu trend data. We'll be building a time series model to make predictions on flu trend.",
   number: 14,
   enabled: true,
   uid: 14,
-  difficulty: 'Intermediate',
+  difficulty: 'Advanced',
   category: Project::COMING_SOON,
 )
 
