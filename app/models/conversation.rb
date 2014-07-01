6c9375bedcf454f3f799c7c3408f0503cef6f8fc
Mark Miyashita <negativetwelve@gmail.com>
2014-07-01 00:10:55 -0700

    Add relationships between conversations and users

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,5 +2,7 @@ class Conversation < ActiveRecord::Base
   obfuscate_id spin: ENV['SPIN']
 
   belongs_to :starter, class_name: User
+  has_many :conversation_users
+  has_many :users, through: :conversation_users
   has_many :messages
 end
