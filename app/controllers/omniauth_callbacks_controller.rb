bf19e3bb970685028b07af4c724a5661ffce7979
tristantao <tao.tristan@gmail.com>
2014-07-01 01:36:09 -0700

    adding omniauth call back controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,17 +1,22 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+ # linkedin_options = {
+ #   scope: 'r_fullprofile r_emailaddress',
+ #   fields: ['id', 'email-address', 'first-name', 'last-name', 'headline', 'location', 'industry', 'picture-url', 'public-profile-url', "picture-urls::(original)"]
+ # }
+#  provider :linkedin, app_id,app_secret, linkedin_options
+#  client = LinkedIn::Client.new
+
   def linkedin
+    byebug
     auth = env["omniauth.auth"]
-    @user = User::connect_to_linkedin(request.env["omniauth.auth"],current_user)
+    @user = User.connect_to_linkedin(request.env["omniauth.auth"], current_user)
     if @user.username != nil
-      flash[:notice] = "Success!`"
+      flash[:notice] = "Logged in via LinkedIn!"
       sign_in_and_redirect @user, :event => :authentication
     else
-      #session["devise.linkedin_uid"] = request.env["omniauth.auth"]
-      #redirect_to new_user_registration_url
-      debugger
       byebug
-      redirect_to :linkedin_confirm
-      #redirect_to linkedin_confirm
+      session["devise.linkedin_uid"] = request.env["omniauth.auth"].uid
+      redirect_to show_linkedin_confirm_path
     end
   end
 end
