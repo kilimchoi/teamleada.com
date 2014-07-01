e97b24cecc9495a585987e802ce4078de4752372
tristantao <tao.tristan@gmail.com>
2014-07-01 14:19:04 -0700

    restrict linkedin-confirm view and clean up

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,22 +1,20 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
- # linkedin_options = {
- #   scope: 'r_fullprofile r_emailaddress',
- #   fields: ['id', 'email-address', 'first-name', 'last-name', 'headline', 'location', 'industry', 'picture-url', 'public-profile-url', "picture-urls::(original)"]
- # }
-#  provider :linkedin, app_id,app_secret, linkedin_options
-#  client = LinkedIn::Client.new
 
   def linkedin
-    byebug
-    auth = env["omniauth.auth"]
+    #byebug
+    if env["omniauth.auth"].nil?
+      flash[:error] = "You must be authorized via linkedin to view this page."
+      redirect_to new_login_url
+    end
     @user = User.connect_to_linkedin(request.env["omniauth.auth"], current_user)
     if @user.username != nil
       flash[:notice] = "Logged in via LinkedIn!"
       sign_in_and_redirect @user, :event => :authentication
     else
-      byebug
+      #byebug
       session["devise.linkedin_uid"] = request.env["omniauth.auth"].uid
       redirect_to show_linkedin_confirm_path
     end
   end
+
 end
