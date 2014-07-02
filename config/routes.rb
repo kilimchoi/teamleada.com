629fcde9df7d092b0867d086b350378d26e17c0a
tristantao <tao.tristan@gmail.com>
2014-07-02 15:34:34 -0700

    cleaning up routes comments

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,6 @@ TeamLeada::Application.routes.draw do
 
   match 'surveys/:name', to: 'surveys#show', via: :get
 
-  #devise_for :users, :controllers => { : omniauth_callbacks => "omniauth_callbacks" }
-
   devise_for :users, path: '',
                      path_names: { sign_in: 'login', sign_up: 'sign-up', sign_out: 'logout'},
                      controllers: {
