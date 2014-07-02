a0f5afa338d2524ce244c2d9b2bb0da52bc6b5c8
tristantao <tao.tristan@gmail.com>
2014-07-02 16:23:05 -0700

    updated schmae with new loation column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140701184558) do
+ActiveRecord::Schema.define(version: 20140702232143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -447,6 +447,7 @@ ActiveRecord::Schema.define(version: 20140701184558) do
     t.integer  "skills_count"
     t.string   "name"
     t.string   "country_code"
+    t.string   "location"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
