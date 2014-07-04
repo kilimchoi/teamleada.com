1cf3f2496aaa02e52c7f858c94b714897cd7239b
tristantao <tao.tristan@gmail.com>
2014-07-03 18:29:11 -0700

    refactor omniauth controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,15 +4,15 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if env["omniauth.auth"].nil?
       flash[:error] = "You must be authorized via linkedin to view this page."
       redirect_to new_login_url
-    end
-    @user = User.connect_to_linkedin(request.env["omniauth.auth"], current_user)
-    if @user.username != nil
-      flash[:notice] = "Logged in via LinkedIn!"
-      sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.linkedin_uid"] = request.env["omniauth.auth"].uid
-      redirect_to show_linkedin_confirm_path
+      @user = User.connect_to_linkedin(request.env["omniauth.auth"], current_user)
+      if @user.username != nil
+        flash[:notice] = "Logged in via LinkedIn!"
+        sign_in_and_redirect @user, :event => :authentication
+      else
+        session["devise.linkedin_uid"] = request.env["omniauth.auth"].uid
+        redirect_to show_linkedin_confirm_path
+      end
     end
   end
-
 end
