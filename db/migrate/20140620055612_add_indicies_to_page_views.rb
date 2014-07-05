9c2d722ae9486e612c0720e32b417ffc5e4ff36e
Mark Miyashita <negativetwelve@gmail.com>
2014-07-04 22:38:12 -0700

    Remove page view migrations

--- db/migrate/20140620055612_add_indicies_to_page_views.rb
+++
@@ -1,8 +0,0 @@
-class AddIndiciesToPageViews < ActiveRecord::Migration
-  def change
-    add_index :page_views, :url
-    add_index :page_views, :user_id
-    add_index :page_views, :viewed_user_id
-    add_index :page_views, [:user_id, :viewed_user_id]
-  end
-end
