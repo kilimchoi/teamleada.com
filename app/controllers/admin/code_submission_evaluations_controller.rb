55a7a87beb01c5da70f078575c6f9652e7919976
Mark Miyashita <negativetwelve@gmail.com>
2014-07-06 13:40:30 -0700

    Add code submissions evaluations controller

---
+++ app/controllers/admin/code_submission_evaluations_controller.rb
@@ -0,0 +1,17 @@
+class Admin::CodeSubmissionEvaluationsController < Admin::BaseController
+  load_and_authorize_resource
+
+  def index
+  end
+
+  private
+
+  def evaluation_params
+    params.require(:code_submission_evaluation).permit(:description,
+                                                       :computer_science,
+                                                       :statistics,
+                                                       :curiosity,
+                                                       :communication)
+  end
+
+end
