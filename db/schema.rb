3e70c26f8b1ffb46c8d868757da7f37e9aa2f595
tristantao <tao.tristan@gmail.com>
2014-07-08 16:05:53 -0700

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140708204103) do
+ActiveRecord::Schema.define(version: 20140708205610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -330,6 +330,18 @@ ActiveRecord::Schema.define(version: 20140708204103) do
     t.text     "company_overview"
     t.string   "category"
   end
+
+  create_table "publications", force: true do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "description"
+    t.date     "publication_date"
+    t.string   "publisher"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "publications", ["user_id"], name: "index_publications_on_user_id", using: :btree
 
   create_table "questions", force: true do |t|
     t.text     "description"
