8f63ae4b9330b5f0c9b7d136e9576d29ae71ace6
Mark Miyashita <negativetwelve@gmail.com>
2014-07-10 00:03:44 -0700

    Add photo to profile_photos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140710065954) do
+ActiveRecord::Schema.define(version: 20140710070308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -251,6 +251,7 @@ ActiveRecord::Schema.define(version: 20140710065954) do
     t.datetime "photo_updated_at"
     t.boolean  "photo_processing",   default: false
     t.string   "photo_tmp"
+    t.string   "photo"
   end
 
   create_table "project_interests", force: true do |t|
