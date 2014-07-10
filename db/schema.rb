f5c3162badb036ce07a1c7caafb20fa2ba39ed4c
tristantao <tao.tristan@gmail.com>
2014-07-09 23:50:29 -0700

    adding user_skills table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140710064508) do
+ActiveRecord::Schema.define(version: 20140710064920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -491,6 +491,16 @@ ActiveRecord::Schema.define(version: 20140710064508) do
     t.datetime "updated_at"
     t.integer  "code_id"
   end
+
+  create_table "user_skills", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "skill_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "user_skills", ["skill_id"], name: "index_user_skills_on_skill_id", using: :btree
+  add_index "user_skills", ["user_id"], name: "index_user_skills_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                        default: "",    null: false
