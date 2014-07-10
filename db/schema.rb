a574fda1d80a9eb1521fe3c0d03039999a287320
tristantao <tao.tristan@gmail.com>
2014-07-10 00:49:28 -0700

    updating db schema linkedin_id up change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140710064920) do
+ActiveRecord::Schema.define(version: 20140710074114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -347,7 +347,7 @@ ActiveRecord::Schema.define(version: 20140710064920) do
     t.string   "publisher"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "publication_id"
+    t.string   "linkedin_publication_id"
   end
 
   add_index "publications", ["user_id"], name: "index_publications_on_user_id", using: :btree
@@ -393,7 +393,7 @@ ActiveRecord::Schema.define(version: 20140710064920) do
 
   create_table "skills", force: true do |t|
     t.string   "skill_name"
-    t.string   "skill_id"
+    t.string   "linkedin_skill_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
