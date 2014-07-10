c48e4970b294d5c0fe09dd7400f87323dfafa36f
Mark Miyashita <negativetwelve@gmail.com>
2014-07-10 01:32:04 -0700

    Change column for enabled

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140710080523) do
+ActiveRecord::Schema.define(version: 20140710082305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -133,7 +133,7 @@ ActiveRecord::Schema.define(version: 20140710080523) do
   end
 
   create_table "features", force: true do |t|
-    t.boolean  "enabled?"
+    t.boolean  "enabled"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
