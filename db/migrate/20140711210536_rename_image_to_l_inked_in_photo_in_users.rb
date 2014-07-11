4a3668f8d272abb5623e530dd4ac3c06cf5016a6
tristantao <tao.tristan@gmail.com>
2014-07-11 14:14:38 -0700

    renaming the linkedin img col in User

--- db/migrate/20140711210536_rename_image_to_l_inked_in_photo_in_users.rb
+++ db/migrate/20140711210536_rename_image_to_l_inked_in_photo_in_users.rb
@@ -1,5 +1,6 @@
 class RenameImageToLInkedInPhotoInUsers < ActiveRecord::Migration
   def change
     rename_column :users, :image, :linkedin_prof_image_url
+    change_column :users, :linkedin_prof_image_url, :string, :default => ''
   end
 end
