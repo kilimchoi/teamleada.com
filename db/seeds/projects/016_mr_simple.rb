ca235aa12768fefd80bb122efea400db82803732
tristantao <tao.tristan@gmail.com>
2014-07-20 11:58:58 -0700

    main content

--- db/seeds/projects/016_mr_simple.rb
+++ db/seeds/projects/016_mr_simple.rb
@@ -1,7 +1,14 @@
 
 main_page_content = [
-	['text', 'This is the test project to gauge interest of what kinds of projects our users want.'],
-
+  ['text', "In this project, you'll design and build a mapreduce algorithm to traverse graph data."],
+  ['text', "Graph is a powerful data structure that is used in the following cases:"],
+  ['text', "- Social Data"],
+  ['text', "- Road Networks"],
+  ['text', "- Biological Networks"],
+  ['text', "- Web Networks"],
+  ['text', "Often times, the graph data mentioned above can't be properly analyze via a single computer (Can you think of why?)."],
+  ['text', "Come in distributed system such as Hadoop to help."],
+  ['text', "Here, we'll be doing simple graph traversal via mapreduce to understand the power/limitation of distributed computing."],
 ]
 
 project = Project.create!(
@@ -18,5 +25,28 @@ project = Project.create!(
 ################################################################################
 puts "============ Created project: #{project.title}."
 ################################################################################
+#
+project_overview_content0 = [
+  ['text', "In this project, you'll design and build a mapreduce algorithm to traverse graph data."],
+  ['text', "Graph is a powerful data structure that is used in the following cases:"],
+  ['text', "- Social Data"],
+  ['text', "- Road Networks"],
+  ['text', "- Biological Networks"],
+  ['text', "- Web Networks"],
+  ['text', "Often times, the graph data mentioned above can't be properly analyze via a single computer (Can you think of why?)."],
+  ['text', "Come in distributed system such as Hadoop to help."],
+  ['text', "Here, we'll be doing simple graph traversal via mapreduce to understand the power/limitation of distributed computing."],
+  ['lesson_links', nil],
+]
 
+project_overview = Lesson.create!(
+  title: "Project Overview",
+  project: project,
+  lesson_id: 0,
+)
 
+project_overview_slide0 = Slide.create!(
+  content: project_overview_content0,
+  parent: project_overview,
+  slide_id: 0,
+)
