6cf5d1eb7de362c1adf6a8e4169e44b4a6a3caa9
tristantao <tao.tristan@gmail.com>
2014-07-24 13:19:51 -0700

    finished first slide time series challenge

--- db/seeds/projects/017_flu_trend_challenge.rb
+++ db/seeds/projects/017_flu_trend_challenge.rb
@@ -1,9 +1,10 @@
 
 main_page_content = [
   ['text', "In this project, we'll build off of the flu trend project."],
-  ['text', "You're given a new dataset to build a new time series model."],
-  ['text', "You're given a timeframe, as well as a submission count limit"],
-  ['text', "You'll be evaluated on the complexity of the model and also the accuracy of your predictions"],
+  ['text', "- You're given a new dataset to build a new time series model."],
+  ['text', "- You're given a timeframe, as well as a submission count limit"],
+  ['text', "- You'll be evaluated on the complexity of the model and also the accuracy of your predictions"],
+  ['text', "Good Luck!"],
 ]
 
 project = Project.create!(
@@ -22,6 +23,24 @@ project = Project.create!(
 ################################################################################
 puts "============ Created project: #{project.title}."
 ################################################################################
+main_page_content = [
+  ['text', 'The premise of the project is as follows:'],
+  ['text', "- You are given 3 datasets."],
+  ['text', "- You're asked to fit a time series model on 2 of them (choose 2, and you can ignore the rest)."],
+  ['text', "- You'll have to create a prediction over the next 100 periods on both of the datasets you chose."],
+  ['text', "- Lastly, you're asked to writeup/present your process."],
+  ['text', "- You'll receive a a score on "],
+  ['lesson_links', nil],
+]
 
+main_page_lesson = Lesson.create!(
+  title: "Time Series Challenge: Context",
+  project: project,
+  lesson_id: 0,
+)
 
-
+main_page_slide = Slide.create!(
+  content: main_page_content,
+  parent: main_page_lesson,
+  slide_id: 0,
+)
