b53d2253f93cbe56954eb7d13145cf3b33df9ed3
Mark Miyashita <negativetwelve@gmail.com>
2014-07-30 02:58:42 -0700

    Remove end date from project statuses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730095526) do
+ActiveRecord::Schema.define(version: 20140730095809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -314,7 +314,6 @@ ActiveRecord::Schema.define(version: 20140730095526) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "start_date"
-    t.datetime "end_date"
   end
 
   create_table "projects", id: false, force: true do |t|
