ecac09caff2cde4a9dbeb81f00d648609f7bdd58
Mark Miyashita <negativetwelve@gmail.com>
2014-08-02 10:56:14 -0700

    Annotate user interaction

--- app/models/user_interaction.rb
+++ app/models/user_interaction.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: user_interactions
+#
+#  id                 :integer          not null, primary key
+#  company_id         :integer
+#  interactor_id      :integer
+#  interactee_id      :integer
+#  favorited          :boolean          default(FALSE)
+#  favorited_at       :datetime
+#  contacted          :boolean          default(FALSE)
+#  first_contacted_at :datetime
+#  hired              :boolean          default(FALSE)
+#  hired_at           :datetime
+#  created_at         :datetime
+#  updated_at         :datetime
+#
+
 class UserInteraction < ActiveRecord::Base
 end
--- spec/factories/user_interactions.rb
+++ spec/factories/user_interactions.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: user_interactions
+#
+#  id                 :integer          not null, primary key
+#  company_id         :integer
+#  interactor_id      :integer
+#  interactee_id      :integer
+#  favorited          :boolean          default(FALSE)
+#  favorited_at       :datetime
+#  contacted          :boolean          default(FALSE)
+#  first_contacted_at :datetime
+#  hired              :boolean          default(FALSE)
+#  hired_at           :datetime
+#  created_at         :datetime
+#  updated_at         :datetime
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
