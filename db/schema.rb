b6ed77dff0dd1bdd9fde076f6a788f4fa763643a
Mark Miyashita <negativetwelve@gmail.com>
2014-08-02 10:46:11 -0700

    Drop user favorite user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140802165909) do
+ActiveRecord::Schema.define(version: 20140802174112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -492,14 +492,6 @@ ActiveRecord::Schema.define(version: 20140802165909) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "code_id"
-  end
-
-  create_table "user_favorite_users", force: true do |t|
-    t.integer  "favoriter_id"
-    t.integer  "favoritee_id"
-    t.integer  "company_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "user_skills", force: true do |t|
