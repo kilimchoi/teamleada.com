b98546e0621b097abc3dff4931cfcdb1635942ab
Mark Miyashita <negativetwelve@gmail.com>
2014-08-02 17:20:14 -0700

    Add sorting to user_interaction

--- app/models/user_interaction.rb
+++ app/models/user_interaction.rb
@@ -20,5 +20,18 @@ class UserInteraction < ActiveRecord::Base
   belongs_to :company
   belongs_to :interactor, class_name: User
   belongs_to :interactee, class_name: User
+
+  scope :sort_by_favorited_at, -> { order(:favorited_at) }
+
+  def set_favorited
+    self.favorited = true
+    self.favorited_at = Time.now
+  end
+
+  def set_unfavorited
+    self.favorited = false
+    self.favorited_at = nil
+  end
+
 end
 
