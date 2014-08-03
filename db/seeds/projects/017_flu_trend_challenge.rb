2ee53de26be861f0d12f38aac468c1cc314fba92
Brian Wayne Liou <brian.liou91@gmail.com>
2014-08-02 19:23:30 -0700

    spelling stuff

--- db/seeds/projects/017_flu_trend_challenge.rb
+++ db/seeds/projects/017_flu_trend_challenge.rb
@@ -1,16 +1,16 @@
 
 main_page_content = [
-  ['text', "In this project, we'll build off of the flu trend project."],
-  ['text', "- You're given a new dataset to build a new time series model."],
-  ['text', "- You're given a timeframe, as well as a submission count limit"],
-  ['text', "- You'll be evaluated on the complexity of the model and also the accuracy of your predictions"],
+  ['text', "In this challenge, we'll build off of the flu trends data lesson."],
+  ['text', "You are given a new dataset to build a time series model."],
+  ['text', "You are given a time frame, as well as a submission count limit."],
+  ['text', "You will be evaluated on the complexity of the model and the accuracy of your predictions."],
   ['text', "Good Luck!"],
 ]
 
 project = Project.create!(
   title:"Time Series Challenge",
   description: main_page_content,
-  short_description: "This is a project where you'll be asked to build a time series model on a piece of data. You're free to use any package or language. You'll be judged on the accuracy of the model as well as the complexity of the model.",
+  short_description: "In this challenge you will be tasked to build a time series model on a piece of data. You are free to use any package or language. You will be evaluated on the accuracy of the model as well as it's complexity. You will also submit a short report and presentation of your analysis.",
   number: 17,
   enabled: true,
   uid: 17,
@@ -26,17 +26,16 @@ project = Project.create!(
 puts "============ Created project: #{project.title}."
 ################################################################################
 main_page_content = [
-  ['text', 'The premise of the project is as follows:'],
-  ['text', "- You are given 4 datasets."],
-  ['text', "- You're asked to fit a time series model on 2 of them (choose 2, and you can ignore the rest)."],
-  ['text', "- You'll have to create a prediction over the next 100 periods on both of the datasets you chose."],
-  ['text', "- You'll instantly receive a score on your accuracy."],
-  ['text', "- Lastly, you're asked to writeup/present your process."],
+  ['text', "You are given 4 datasets."],
+  ['text', "You are asked to fit a time series model on 2 of them (choose 2, and you can ignore the rest)."],
+  ['text', "You will have to create predictions over the next 100 periods on both of the datasets you chose."],
+  ['text', "You will instantly receive a score on your accuracy."],
+  ['text', "Lastly, you are tasked with a writeup and presentation of your process."],
   ['next_steps', nil],
 ]
 
 main_page_lesson = Lesson.create!(
-  title: "Time Series Challenge: Context",
+  title: "Challenge Details",
   project: project,
   lesson_id: 0,
 )
@@ -52,15 +51,15 @@ main_page_slide = Slide.create!(
 ################################################################################
 
 deliverables_content_one = [
-  ['text', "Deliverables:"],
-  ['text', "- Src Code for your prediction."],
-  ['text', "- 2 separate prediction for the 2 datasets."],
-  ['text', "- Reporting for your result / process."],
+  ['text', "You will submit the following deliverables:"],
+  ['text', "All source code which you used to make your predictions."],
+  ['text', "Two separate predictions for the 2 datasets you selected."],
+  ['text', "Reporting for your result and a presentation of your findings."],
   ['next_steps', nil],
 ]
 
 deliverables_lesson = Lesson.create!(
-  title: "Time Series Challenge: Deliverables",
+  title: "Challenge Deliverables",
   project: project,
   lesson_id: 1,
 )
@@ -76,7 +75,7 @@ main_page_slide = Slide.create!(
 ################################################################################
 
 data_content_one = [
-  ['text', "The data for thsi project is located at:"],
+  ['text', "The data for this project is located at:"],
   ['link', "https://s3.amazonaws.com/leada/time_series_challenge/challenge_data.csv"],
   ['text', "The data comes in csv format and includes headers."],
   ['text', "All datasets have undergone a certain level of normalization."],
@@ -84,7 +83,7 @@ data_content_one = [
 ]
 
 data_lesson = Lesson.create!(
-  title: "Time Series Challenge: Data",
+  title: "Data",
   project: project,
   lesson_id: 2,
 )
@@ -100,13 +99,13 @@ main_page_slide = Slide.create!(
 ################################################################################
 
 submission_content_one = [
-  ['text', "There are 3 different primary submission module below."],
+  ['text', "There are 3 different submissions you will need to make to complete this challenge."],
   ['text', "There will be more details at each level:"],
   ['lesson_links', nil],
 ]
 
 deliverables_lesson = Lesson.create!(
-  title: "Time Series Challenge: Submissions",
+  title: "Submissions",
   project: project,
   lesson_id: 3,
 )
@@ -120,15 +119,15 @@ main_page_slide = Slide.create!(
 ################################################################################
 
 submission_src_content_one = [
-  ['text', "Paste in your src code below:"],
-  ['text', "Make sure to have concise and informative comments."],
-  ['text', "If you used any non-standard packages, expeain the sources in the comments."],
+  ['text', "Paste in all your source code below:"],
+  ['text', "Be sure to have concise and informative comments."],
+  ['text', "If you used any non-standard packages, explain the sources in the comments."],
   ['user_code', ""],
   ['next_steps', nil],
 ]
 
 submission_src_step = Step.create!(
-  title: "Src Submission",
+  title: "Source Code",
   lesson: deliverables_lesson,
   step_id: 0,
 )
@@ -150,22 +149,23 @@ timeseries_src_submission = SubmissionContext.create!(
 ################################################################################
 
 submission_prediction_content_one = [
-  ['text', "Paste in your 1st prediction below:"],
-  ['text', "First line should specify which series you're predicting."],
+  ['text', "Paste in your first set of predictions below:"],
+  ['text', "The first line should specify which series you are predicting."],
   ['text', "From the 2nd line, include a single value per line, with comma optional (the submission should be 101 lines.)"],
   ['user_code', ""],
+  ['next_steps', nil],
 ]
 
 submission_prediction_content_two = [
-  ['text', "Paste in your 2nd prediction below:"],
-  ['text', "First line should specify which series you're predicting."],
+  ['text', "Paste in your second set of predictions below:"],
+  ['text', "The first line should specify which series you are predicting."],
   ['text', "From the 2nd line, include a single value per line, with comma optional (the submission should be 101 lines.)"],
   ['user_code', ""],
   ['next_steps', nil],
 ]
 
 submission_prediction_step = Step.create!(
-  title: "Prediction Submission",
+  title: "Predictions",
   lesson: deliverables_lesson,
   step_id: 1,
 )
@@ -208,7 +208,7 @@ submission_reporting_content_one = [
 ]
 
 submission_reporting_step = Step.create!(
-  title: "Time Series: Reporting",
+  title: "Report and Presentation",
   lesson: deliverables_lesson,
   step_id: 2,
 )
@@ -230,7 +230,7 @@ submission_reporting_submission = SubmissionContext.create!(
 ################################################################################
 
 conclusion_content_one = [
-  ['text', "Congratz! You're done!"],
+  ['text', "Congratulations! You are done! When you are sure your submissions are complete click the complete project button to provide feedback!"],
   ['finish_project_button', 'http://www.surveygizmo.com/s3/1654603/Project-Feedback-Form'],
 ]
 
