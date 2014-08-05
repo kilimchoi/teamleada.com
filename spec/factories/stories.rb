57e347f2a4a6990f856028a65bd51f58517d735e
Mark Miyashita <negativetwelve@gmail.com>
2014-08-05 01:27:01 -0700

    Annotate story models

--- spec/factories/stories.rb
+++ spec/factories/stories.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: stories
+#
+#  id            :integer          not null, primary key
+#  interactor_id :integer
+#  interactee_id :integer
+#  company_id    :integer
+#  text          :text
+#  created_at    :datetime
+#  updated_at    :datetime
+#  type          :string(255)
+#  favorite      :boolean          default(TRUE)
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
