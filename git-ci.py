67aa06ad85667aa5ba6bdab50c083afa3b41bcf9
Mark Miyashita <negativetwelve@gmail.com>
2014-08-12 01:33:50 -0700

    Squashed 'scripts/' changes from f8f5f23..2ff45bb
    2ff45bb Get current branch working
    366cb1d Ignore pyc files
    80c5364 Get correct output
    20da29b Add default pull commit message
    d859095 Pull with no commit
    1cb7b40 Add current branch function
    182f950 Remove * from print statements
    f321bb7 Why am I calling git twice
    7eae748 Put stars around print statements
    258a95e Fix syntax error
    811c068 Add process and call checks
    26cade1 Remove trailing slash
    d70186f Fix the dashes for unicode
    ce03ca9 Print list of args
    2abf2c5 Add * around the commit message
    3df6fc7 Change git ci steps to push
    7dd57d8 Change subtree commands
    af13a1e Add git ci script
    3932270 Pull changes from primary repo as well
    a2fa29b Remove temp file
    55271dd Add git subtree command for scripts
    4a6c04d Add content to scripts temp file
    30ade57 Add temp file
    a4c05ed Merge branch 'master' of github.com:teamleada/scripts
    ab47139 Add primary git subtree merge
    6b5014d Merge commit '683d39b04949981c55629d6e1344246a94df638b'
    683d39b Print out the full command
    a0c39da Convert tuple to list
    ad239f2 Add utf-8 coding at the top of the python files
    184637f Fix syntax error
    e1a60c1 Add git subtree pushing and pulling
    5927498 test
    a39b999 Add hash bang at the top
    1fdcc04 Add test git up command
    1e5bfd7 Add init for scripts directory
    c53555e Add git up.py and make the directory called LEADA
    e922936 Merge commit 'a1717e4cd2a1758301682a6c02b46d8429c57496'
    1f0515c Add basic git up bash script
    a1717e4 Add git main command
    
    git-subtree-dir: scripts
    git-subtree-split: 2ff45bbb794baf244eb709477478c7cb06d8ed6b

---
+++ git-ci.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from git import *
+
+def git_ci():
+    git_fetch()
+    git_subtree_push("scripts", "scripts")
+    git_subtree_push("primary", "app/assets/stylesheets/primary/")
+
+    branches = ["master"]
+    previous_branch = None
+    for branch in branches:
+        git("checkout", branch)
+        git("merge origin/%s" % branch)
+        if previous_branch:
+            git("merge", previous_branch)
+        previous_branch = branch
+
+if __name__ == '__main__':
+    git_ci()
+
