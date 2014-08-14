0dcdd49bf645e6647ebdc6a66687ea2238a39eea
Mark Miyashita <negativetwelve@gmail.com>
2014-08-14 02:36:47 -0700

    Actually add content to the email

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,6 +2,7 @@ class MessageMailer < ActionMailer::Base
   default from: "noreply@teamleada.com"
 
   def message_email(message, sender, receiver)
+    @message = message
     @receiver = receiver
     @sender = sender
     mail(to: @receiver.email, subject: "[Leada] #{@sender.name} sent you a message")
