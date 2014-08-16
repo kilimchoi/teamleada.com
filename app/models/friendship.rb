d0aba65483324a8f4213ac4d057f5f79be346820
Mark Miyashita <negativetwelve@gmail.com>
2014-08-16 16:57:44 -0700

    Annotate friendship and user model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: friendships
+#
+#  id           :integer          not null, primary key
+#  requester_id :integer
+#  requestee_id :integer
+#  status       :string(255)
+#  requested_at :datetime
+#  accepted_at  :datetime
+#  declined_at  :datetime
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 class Friendship < ActiveRecord::Base
 end
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: friendships
+#
+#  id           :integer          not null, primary key
+#  requester_id :integer
+#  requestee_id :integer
+#  status       :string(255)
+#  requested_at :datetime
+#  accepted_at  :datetime
+#  declined_at  :datetime
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 require 'spec_helper'
 
 describe Friendship do
