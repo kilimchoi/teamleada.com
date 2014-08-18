742a77723a55434f55ab260a99113cd78fd5e260
Mark Miyashita <negativetwelve@gmail.com>
2014-08-18 12:00:41 -0700

    Comment out method

--- app/extended_classes/hash.rb
+++ app/extended_classes/hash.rb
@@ -1,17 +1,17 @@
 class Hash
 
-  def symbolize_keys
-    # Borrowed from:
-    # https://gist.github.com/Integralist/9503099
-    return self.reduce({}) do |memo, (k, v)|
-      memo.tap { |m| m[k.to_sym] = v.symbolize_keys }
-    end if self.is_a? Hash
-
-    return self.reduce([]) do |memo, v|
-      memo << v.symbolize_keys; memo
-    end if self.is_a? Array
-
-    self
-  end
+#  def symbolize_keys
+#    # Borrowed from:
+#    # https://gist.github.com/Integralist/9503099
+#    return self.reduce({}) do |memo, (k, v)|
+#      memo.tap { |m| m[k.to_sym] = v.symbolize_keys }
+#    end if self.is_a? Hash
+#
+#    return self.reduce([]) do |memo, v|
+#      memo << v.deep_symbolize_keys; memo
+#    end if self.is_a? Array
+#
+#    self
+#  end
 
 end
