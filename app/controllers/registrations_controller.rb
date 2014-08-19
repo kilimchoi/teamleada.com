3021060091a4f6775ee40350a3c348b666db848e
Mark Miyashita <negativetwelve@gmail.com>
2014-08-19 16:23:13 -0700

    Add debug info message

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,13 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     @sign_up_form = SignUpForm.new
     if @sign_up_form.submit(params[:sign_up_form])
+      # TODO(mark): Move this message creation to some other sort of object...
+      message = "A message with a confirmation link has been sent to your email address. Please click on the link to activate your account."
+      if Rails.env.development?
+        secret = @sign_up_form.user.generate_new_token
+        message += " DEVELOPMENT MODE: <a href='" + user_confirmation_url(confirmation_token: secret) + "'>Activate</a>"
+      end
+      flash[:info] = message.html_safe
 
       redirect_to after_sign_up_path_for(@sign_up_form.user)
     else
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,6 @@ class SessionsController < Devise::SessionsController
   def create
     session[:return_to] ||= request.referer
 
-    puts 'before'
     self.resource = warden.authenticate(auth_options)
     if resource.nil?
       flash[:danger] = "Invalid username or password."
