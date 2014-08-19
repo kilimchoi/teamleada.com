485a4d868dd221e93a0839eea65292b2af891e99
Mark Miyashita <negativetwelve@gmail.com>
2014-08-19 15:11:40 -0700

    Set some attributes using the sign up form

---
+++ app/forms/sign_up_form.rb
@@ -0,0 +1,38 @@
+class SignUpForm < Form
+
+  delegate :email, to: :user
+
+  def initialize
+  end
+
+  def user
+    @user ||= User.new
+  end
+
+  def submit(params)
+    self.set_attributes(params)
+
+    if valid?
+      # save updates
+      true
+    else
+      false
+    end
+  end
+
+  def set_attributes(params)
+    self.email = params[:email]
+    # first_name
+    # last_name
+  end
+
+  #
+  # Validations
+  #
+  def validate_unique_email
+    if User.exists? self.email
+      error.add()
+    end
+  end
+
+end
