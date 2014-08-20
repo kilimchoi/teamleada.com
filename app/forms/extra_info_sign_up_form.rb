1e52d8a0fa983a578b09a4257a9a0e2dc630fef0
Mark Miyashita <negativetwelve@gmail.com>
2014-08-20 09:35:41 -0700

    Skip confirmation page, just confirm right away

---
+++ app/forms/extra_info_sign_up_form.rb
@@ -0,0 +1,29 @@
+class ExtraInfoSignUpForm < Form
+
+  delegate :bio, :date_of_birth, :location, :looking_for_opportunities, to: :user_profile
+
+  def initialize(user)
+    @user = user
+  end
+
+  def user_profile
+    @user_profile ||= @user.profile
+  end
+
+  def set_attributes(params)
+    user_profile.bio                       = params[:bio]
+    user_profile.date_of_birth             = params[:date_of_birth]
+    user_profile.location                  = params[:location]
+    user_profile.looking_for_opportunities = params[:looking_for_opportunities]
+  end
+
+  def save
+    user_profile.save
+  end
+
+  #
+  # Validations
+  #
+
+end
+
