28c822603283a7bdd92458e2bc54141eb703e6e2
Mark Miyashita <negativetwelve@gmail.com>
2014-08-20 00:53:39 -0700

    Annotate the user profile model

--- spec/factories/user_profiles.rb
+++ spec/factories/user_profiles.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: user_profiles
+#
+#  id                        :integer          not null, primary key
+#  user_id                   :integer
+#  looking_for_opportunities :boolean          default(TRUE)
+#  location                  :string(255)
+#  bio                       :text
+#  phone                     :string(255)
+#  headline                  :string(255)
+#  industry                  :string(255)
+#  date_of_birth             :date
+#  interests                 :text
+#  job_bookmarks_count       :integer
+#  country_code              :string(255)
+#  created_at                :datetime
+#  updated_at                :datetime
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
