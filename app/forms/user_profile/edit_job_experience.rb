1fb700074580ec57d4b60aa1789bf5a55f0c10c4
Mark Miyashita <negativetwelve@gmail.com>
2014-08-21 17:06:09 -0700

    Add specific forms for each component of theuser profile

---
+++ app/forms/user_profile/edit_job_experience.rb
@@ -0,0 +1,50 @@
+class EditProfileForm < Form
+
+  delegate :summary, to: :job_experience, prefix: true
+  delegate :name, to: :company, prefix: true
+
+  validates :company_name, presence: true, if: @edited_job_experience
+
+  def initialize(user, params={})
+    @user = user
+    @job_experience = user.job_experiences.find(params[:job_experience_id]) if params[:job_experience_id].present?
+    @company = job_experience.company || Company.find_by(name: params[:company_name]) if params[:company_name].present?
+  end
+
+  def job_experience
+    # We don't want to build the association on the user in case the object isn't saved
+    @job_experience ||= JobExperience.new
+  end
+
+  def company
+    @company ||= Company.new
+  end
+
+  def set_attributes(params)
+    set_job_experience(params)
+    set_company(params)
+  end
+
+  def set_job_experience(params)
+    job_experience.summary = params[:job_experience_summary] if params[:job_experience_summary].present?
+    job_experience.user    = @user
+
+    if params[:company_name].present?
+      @company = Company.find_by(name: params[:company_name]) || Company.new
+    end
+
+    job_experience.company = @company
+  end
+
+  def set_company(params)
+    company.name = params[:company_name] if params[:company_name].present?
+  end
+
+  def save
+    # Must save the company first, otherwise the relationship won't exist for the job_experience
+    company.save
+    job_experience.save
+  end
+
+end
+
