32f904be305938d7f492b3895187f78daf8965de
Mark Miyashita <negativetwelve@gmail.com>
2014-08-22 10:56:01 -0700

    Load all forms for each method in the controller

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,21 +1,20 @@
 class UserProfilesController < ApplicationController
   before_filter :set_user
+  before_filter :set_forms
 
   def update_about
-    @edit_about_form = EditAboutForm.new(@user, params[:edit_about_form])
     if @edit_about_form.submit(params[:edit_about_form])
-      respond_to { |format| format.js }
+      respond_to { |format| format.js { render partial: "users/update_profile" } }
     else
-      respond_to { |format| format.js }
+      respond_to { |format| format.js { render partial: "users/update_profile" } }
     end
   end
 
   def update_job_experience
-    @edit_job_experience_form = EditJobExperienceForm.new(@user, params[:edit_job_experience_form])
     if @edit_job_experience_form.submit(params[:edit_job_experience_form])
-      respond_to { |format| format.js }
+      respond_to { |format| format.js { render partial: "users/update_profile" } }
     else
-      respond_to { |format| format.js }
+      respond_to { |format| format.js { render partial: "users/update_profile" } }
     end
   end
 
@@ -23,6 +22,11 @@ class UserProfilesController < ApplicationController
 
   def set_user
     @user = current_user
+  end
+
+  def set_forms
+    @edit_about_form = EditAboutForm.new(@user)
+    @edit_job_experience_form = EditJobExperienceForm.new(@user, params[:edit_job_experience_form])
   end
 
 end
