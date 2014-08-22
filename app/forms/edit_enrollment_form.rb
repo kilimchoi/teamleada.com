13d1b5606f18fb8b3edeb4d6ebfedd4c97228754
Mark Miyashita <negativetwelve@gmail.com>
2014-08-22 12:04:21 -0700

    Add setting attributes and saving to the edit enrollment form

--- app/forms/edit_enrollment_form.rb
+++ app/forms/edit_enrollment_form.rb
@@ -1,14 +1,40 @@
 class EditEnrollmentForm < Form
 
+  delegate :name, to: :university, prefix: true
+
   def initialize(user, params={})
     params ||= {}
     @user = user
+    @enrollment = user.enrollments.find(params[:enrollment_id]) if params[:enrollment_id].present?
+    @university = enrollment.university || University.find_by_university_params(params)
+  end
+
+  def enrollment
+    @enrollment ||= Enrollment.new
+  end
+
+  def university
+    @university ||= University.new
   end
 
   def set_attributes(params)
+    set_university(params)
+    set_enrollment(params)
+  end
+
+  def set_enrollment(params)
+    enrollment.degree = params[:degree] if params[:degree].present?
+    enrollment.field_of_study = params[:field_of_study] if params[:field_of_study].present?
+    enrollment.notes = params[:notes] if params[:notes].present?
+  end
+
+  def set_university(params)
+    university.name = params[:university_name] if params[:university_name].present?
   end
 
   def save
+    university.save
+    enrollment.save
   end
 
 end
