9d518c46e2fa6d116ffead9f2f15ec87f55141f4
Mark Miyashita <negativetwelve@gmail.com>
2014-08-22 10:55:28 -0700

    Properly save the user job

--- app/forms/edit_job_experience_form.rb
+++ app/forms/edit_job_experience_form.rb
@@ -1,19 +1,27 @@
 class EditJobExperienceForm < Form
 
-  delegate :summary, to: :job_experience, prefix: true
   delegate :name, to: :company, prefix: true
+  delegate :summary, to: :job_experience, prefix: true
+  delegate :location, to: :job, prefix: true
+  delegate :position_title, to: :job, prefix: true
 
   validates :company_name, presence: true, if: @edited_job_experience
 
   def initialize(user, params={})
+    params ||= {}
     @user = user
     @job_experience = user.job_experiences.find(params[:job_experience_id]) if params[:job_experience_id].present?
-    @company = job_experience.company || Company.find_by(name: params[:company_name]) if params[:company_name].present?
+    @job = job_experience.job || Job.find_by_job_params(params)
+    @company = job_experience.company || Company.find_by_company_params(params)
   end
 
   def job_experience
     # We don't want to build the association on the user in case the object isn't saved
     @job_experience ||= JobExperience.new
+  end
+
+  def job
+    @job ||= Job.new
   end
 
   def company
@@ -22,6 +30,7 @@ class EditJobExperienceForm < Form
 
   def set_attributes(params)
     set_job_experience(params)
+    set_job(params)
     set_company(params)
   end
 
@@ -36,6 +45,11 @@ class EditJobExperienceForm < Form
     job_experience.company = @company
   end
 
+  def set_job(params)
+    job.position_title = params[:job_position_title] if params[:job_position_title].present?
+    job.location       = params[:job_location] if params[:job_location].present?
+  end
+
   def set_company(params)
     company.name = params[:company_name] if params[:company_name].present?
   end
@@ -43,6 +57,7 @@ class EditJobExperienceForm < Form
   def save
     # Must save the company first, otherwise the relationship won't exist for the job_experience
     company.save
+    job.save
     job_experience.save
   end
 
