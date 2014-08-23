6b6f85d5ec9ac7f3a08e9a999fe39d80417c6adb
Mark Miyashita <negativetwelve@gmail.com>
2014-08-23 00:02:42 -0700

    Add route for cancelling the update

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,7 @@ TeamLeada::Application.routes.draw do
   match "profile", to: "users#profile", as: "show_user_profile", via: :get
   match "profile/edit", to: "users#edit_profile", as: "edit_user_profile", via: :get
 
+  match "profile/edit/cancel", to: "user_profiles#cancel_update", as: "cancel_update_user_profile", via: :post
   match "profile/edit/about", to: "user_profiles#update_about", as: "update_about_user_profile", via: :post
   match "profile/edit/job_experience", to: "user_profiles#update_job_experience", as: "update_job_experience_user_profile", via: :post
   match "profile/edit/job_experience", to: "user_profiles#remove_job_experience", as: "remove_job_experience_user_profile", via: :delete
