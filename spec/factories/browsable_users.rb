18375d44ac60ab77435f06acd566decb9351e29a
Mark Miyashita <negativetwelve@gmail.com>
2014-08-24 18:22:10 -0700

    Annotate browsable users

--- spec/factories/browsable_users.rb
+++ spec/factories/browsable_users.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: browsable_users
+#
+#  id         :integer          not null, primary key
+#  company_id :integer
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
--- spec/models/browsable_user_spec.rb
+++ spec/models/browsable_user_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: browsable_users
+#
+#  id         :integer          not null, primary key
+#  company_id :integer
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe BrowsableUser do
