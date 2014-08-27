c334b5b30df72a070a1bc64ad377538aa662add0
tristantao <tao.tristan@gmail.com>
2014-08-26 18:50:43 -0700

    adding migration to add project ref to quizzes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827011031) do
+ActiveRecord::Schema.define(version: 20140827013607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -398,6 +398,7 @@ ActiveRecord::Schema.define(version: 20140827011031) do
     t.string   "answer"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "project_id"
   end
 
   create_table "resumes", force: true do |t|
