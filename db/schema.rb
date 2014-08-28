6330fc077b7e787d32eb9db3fec01cd51262e9f1
Mark Miyashita <negativetwelve@gmail.com>
2014-08-28 10:57:30 -0700

    Update schema with project submission

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828005427) do
+ActiveRecord::Schema.define(version: 20140828175621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -335,6 +335,13 @@ ActiveRecord::Schema.define(version: 20140828005427) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "start_date"
+  end
+
+  create_table "project_submissions", force: true do |t|
+    t.integer  "project_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "projects", id: false, force: true do |t|
