02857a28aa0f6ef8e02e96d2b2486a0ae522a879
Mark Miyashita <negativetwelve@gmail.com>
2014-08-28 11:02:39 -0700

    Add project submission uploader

--- app/models/project_submission.rb
+++ app/models/project_submission.rb
@@ -10,6 +10,44 @@
 #
 
 class ProjectSubmission < ActiveRecord::Base
+  include ::CarrierWave::Backgrounder::Delay
+  mount_uploader :upload_file, ProjectSubmissionUploader
+  process_in_background :upload_file
+  skip_callback :save, :after, :remove_previously_stored_upload_file
+
   belongs_to :project
   belongs_to :user
+
+  belongs_to :user
+
+  default_scope { order(:created_at) }
+
+  def pretty_upload_date
+    created_at.strftime("%B %d, %Y")
+  end
+
+  def url
+    resume_file_tmp_url || resume_file.url
+  end
+
+  def resume_file_tmp_url
+    "/tmp/uploads/#{resume_file_tmp}" unless resume_file_tmp.nil?
+  end
+
+  def first_name
+    user.first_name
+  end
+
+  def last_name
+    user.last_name
+  end
+
+  def version
+    user.resumes.index(self) + 1
+  end
+
+  def filename
+    original_filename
+  end
+
 end
