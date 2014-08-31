b6a202c076a0d018622abdb4c9face02dbd00b05
Mark Miyashita <negativetwelve@gmail.com>
2014-08-30 18:07:25 -0700

    Annotate user action model

--- app/models/user_action.rb
+++ app/models/user_action.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: user_actions
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class UserAction < ActiveRecord::Base
 end
