a6994e0483d2ea380e7d1f5c11d0c80ee97a5cc6
Mark Miyashita <negativetwelve@gmail.com>
2014-09-01 16:22:04 -0700

    Change path of users to 'u'

--- config/routes/users_routes.rb
+++ config/routes/users_routes.rb
@@ -1,6 +1,6 @@
 TeamLeada::Application.routes.draw do
 
-  resources :users, only: [:show, :update] do
+  resources :users, path: "u", only: [:show, :update] do
 
     scope module: :users do
       resources :projects, only: [:show, :index] do
@@ -8,6 +8,7 @@ TeamLeada::Application.routes.draw do
           match "feedback", to: "projects#feedback", as: :feedback, via: :get
         end
       end
+      resources :story_notifications, path: "posts", only: [:show]
     end
 
   end
