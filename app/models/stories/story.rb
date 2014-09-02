6839f262e9a2f9fc64367a9d55007d751015800a
Mark Miyashita <negativetwelve@gmail.com>
2014-09-01 23:18:53 -0700

    Annotate stories with new action_object

--- app/models/stories/story.rb
+++ app/models/stories/story.rb
@@ -2,15 +2,15 @@
 #
 # Table name: stories
 #
-#  id           :integer          not null, primary key
-#  subject_id   :integer
-#  subject_type :string(255)
-#  object_id    :integer
-#  object_type  :string(255)
-#  type         :string(255)
-#  created_at   :datetime
-#  updated_at   :datetime
-#  data         :text
+#  id                 :integer          not null, primary key
+#  subject_id         :integer
+#  subject_type       :string(255)
+#  type               :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  data               :text
+#  action_object_id   :integer
+#  action_object_type :string(255)
 #
 
 class Story < ActiveRecord::Base
