e81e515640e3cfb941f7caa7028546abcf0962ea
Mark Miyashita <negativetwelve@gmail.com>
2014-09-02 11:10:23 -0700

    Add user routes for showing posts

--- config/routes/users_routes.rb
+++ config/routes/users_routes.rb
@@ -8,7 +8,7 @@ TeamLeada::Application.routes.draw do
           match "feedback", to: "projects#feedback", as: :feedback, via: :get
         end
       end
-      resources :stories, path: "posts", only: [:show]
+      resources :stories, path: "posts", only: [:show, :index]
     end
 
   end
