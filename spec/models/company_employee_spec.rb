6e94affde1a5eb0dc0550ae2c2a594642b7b1aba
Mark Miyashita <negativetwelve@gmail.com>
2014-09-04 11:15:30 -0700

    Annotate models

--- spec/models/company_employee_spec.rb
+++ spec/models/company_employee_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: company_employees
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  company_id :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe CompanyEmployee do
