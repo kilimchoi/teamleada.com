1c337c7ab73c36ef7887eaed3d0f65c91be137ec
Mark Miyashita <negativetwelve@gmail.com>
2014-09-05 15:05:36 -0700

    Annotate company interest models

--- app/models/company_interest.rb
+++ app/models/company_interest.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: company_interests
+#
+#  id         :integer          not null, primary key
+#  company_id :integer
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class CompanyInterest < ActiveRecord::Base
 end
