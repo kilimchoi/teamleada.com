b783c3bcd5a8b374a5bdaf36fbb05d9b9375b5ab
Mark Miyashita <negativetwelve@gmail.com>
2014-09-08 12:59:34 -0700

    Find code submission evaluations for a specific user/project/code_submission

--- app/controllers/admin/users/projects/code_submissions/code_submission_evaluations_controller.rb
+++ app/controllers/admin/users/projects/code_submissions/code_submission_evaluations_controller.rb
@@ -2,7 +2,7 @@ class Admin::Users::Projects::CodeSubmissions::CodeSubmissionEvaluationsControll
   load_and_authorize_resource :code_submission_evaluation
 
   def index
-
+    @code_submission_evaluations = @user.evaluations_for_project(@project).where(code_submission: @code_submission)
   end
 
 end
