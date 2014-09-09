c38fa3639cbd2e40025c283cb4ac644f3d3a0c1d
Mark Miyashita <negativetwelve@gmail.com>
2014-09-09 14:16:49 -0700

    Create and annotate the project_submission model

---
+++ app/models/project_submission.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: project_submissions
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer
+#  project_id   :integer
+#  slide_id     :integer
+#  content_id   :integer
+#  content_type :string(255)
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
+class ProjectSubmission < ActiveRecord::Base
+end
