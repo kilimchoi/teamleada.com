113142322ba11e570f8395a663f4934cb90ec233
Mark Miyashita <negativetwelve@gmail.com>
2014-09-10 10:07:45 -0700

    Annotate free response submissions

--- spec/models/free_response_submission_content_spec.rb
+++ spec/models/free_response_submission_content_spec.rb
@@ -3,7 +3,6 @@
 # Table name: free_response_submission_contents
 #
 #  id         :integer          not null, primary key
-#  user_id    :integer
 #  content    :text
 #  created_at :datetime
 #  updated_at :datetime
