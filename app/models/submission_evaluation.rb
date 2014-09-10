7b06bb7c34602ea31e793e10b4c32318bb2f0d21
Mark Miyashita <negativetwelve@gmail.com>
2014-09-09 17:29:58 -0700

    Add relationships to submission_evaluations

--- app/models/submission_evaluation.rb
+++ app/models/submission_evaluation.rb
@@ -1,2 +1,7 @@
 class SubmissionEvaluation < ActiveRecord::Base
+  belongs_to :reviewer, class_name: User
+  belongs_to :reviewee, class_name: User
+  belongs_to :project
+  belongs_to :project_submission
+  belongs_to :content, polymorphic: true
 end
