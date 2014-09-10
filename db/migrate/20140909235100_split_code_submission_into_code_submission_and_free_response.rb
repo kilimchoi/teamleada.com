c3d563c4b9ab9b7d04738c34e677fd65fa8010c5
Mark Miyashita <negativetwelve@gmail.com>
2014-09-09 19:52:36 -0700

    Alternate way of getting access to the slide

--- db/migrate/20140909235100_split_code_submission_into_code_submission_and_free_response.rb
+++ db/migrate/20140909235100_split_code_submission_into_code_submission_and_free_response.rb
@@ -1,7 +1,12 @@
 class SplitCodeSubmissionIntoCodeSubmissionAndFreeResponse < ActiveRecord::Migration
   def change
     CodeSubmissionContent.all.each do |code_submission|
-      if code_submission.submission_context.submission_type != "code"
+      parent_class = code_submission.parent_type == "Step" ? Step : Lesson
+      parent = parent_class.find(code_submission.parent_id)
+      project_id = parent.project.id
+      slide = parent.slides[code_submission.slide_index]
+
+      if slide.submission_context.submission_type != "code"
         FreeResponseSubmissionContent.create(
           user_id: code_submission.user_id,
           project_submission_id: code_submission.project_submission_id,
