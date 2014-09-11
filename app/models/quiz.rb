6bb22428a43e415e54e5050b9130ee8a824ed4dc
Mark Miyashita <negativetwelve@gmail.com>
2014-09-11 10:25:01 -0700

    Fix style in the quiz model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -11,35 +11,40 @@
 #
 
 class Quiz < ActiveRecord::Base
-  belongs_to :project
-  validates :project, :presence => true
-
   self.primary_key = "quiz_id"
+
+  belongs_to :project
   has_many :quiz_submissions
 
+  validates :project, presence: true
   validates :quiz_id, uniqueness: true, presence: true
 
+  class << self
+    def lower_and_clean(input)
+      # Given an input, loswercases and cleans white spaces.
+      input.to_s.downcase.gsub(/\s+/, "")
+    end
+
+    def sanitize_answer(answer)
+      ActionController::Base.helpers.sanitize(answer)
+    end
+  end
+
   def correct_answer?(user_input)
-    '''
-    Given a "user_input", returns True if the user_input matches the answer.
-    '''
+    # Given a "user_input", returns True if the user_input matches the answer.
     answer == user_input || whitespace_and_lower_pass(user_input) || equalsign_pass(user_input) || quoteless_pass(user_input)
   end
 
   def quoteless_pass(user_input)
-    '''
-    Returns T/F depending of answer == user_input, after we remove all quotes.
-    '''
-    quote_removed_input = Quiz.lower_and_clean(user_input).gsub(/'|"/, "") 
-    quote_removed_answer = Quiz.lower_and_clean(answer).gsub(/'|"/, "") 
+    # Returns T/F depending of answer == user_input, after we remove all quotes.
+    quote_removed_input = Quiz.lower_and_clean(user_input).gsub(/'|"/, "")
+    quote_removed_answer = Quiz.lower_and_clean(answer).gsub(/'|"/, "")
 
     quote_removed_input == quote_removed_answer
   end
 
   def whitespace_and_lower_pass(user_input)
-    '''
-    Returns T/F depending of answer == user_input, after we remove all white spaces.
-    '''
+    # Returns T/F depending of answer == user_input, after we remove all white spaces.
     space_removed_input = Quiz.lower_and_clean(user_input) #user_input.gsub(/\s+/, "")
     space_removed_answer = Quiz.lower_and_clean(answer) #answer.gsub(/\s+/, "")
 
@@ -47,31 +52,14 @@ class Quiz < ActiveRecord::Base
   end
 
   def equalsign_pass(user_input)
-    '''
-    Return T/F while taking into account that "<-" and "=" are the same
-    Removes white spaces too
-    '''
+    # Return T/F while taking into account that "<-" and "=" are the same
+    # Removes white spaces too
     space_removed_input = Quiz.lower_and_clean(user_input) #user_input.gsub(/\s+/, "")
     space_removed_answer = Quiz.lower_and_clean(answer) #answer.gsub(/\s+/, "")
 
     equalized_input = space_removed_input.gsub('<-', '=')
     equalized_answer = space_removed_answer.gsub('<-', '=')
     equalized_input == equalized_answer
-  end
-
-  ###################
-  ###Static Methods##
-  ###################
-  
-  def self.lower_and_clean(input)
-    '''
-    Given an input, loswercases and cleans white spaces.
-    '''
-    input.to_s.downcase.gsub(/\s+/, "")
-  end
-
-  def self.sanitize_answer(answer)
-    return ActionController::Base.helpers.sanitize(answer)
   end
 
 end
