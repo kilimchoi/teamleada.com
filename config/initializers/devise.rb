ed1a42d0e88f40af392204775a6f01d5a3195b3f
Mark Miyashita <negativetwelve@gmail.com>
2014-09-11 13:27:38 -0700

    Remove devise password check, use sign_up_form exclusively

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -136,7 +136,8 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 8..128
+  # We're doing our own validation in the sign_up_form.rb
+  # config.password_length = 6..128
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
