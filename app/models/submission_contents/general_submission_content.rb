8957846e58ea8d03cabbc26bcc2e5032313bf37f
Mark Miyashita <negativetwelve@gmail.com>
2014-09-12 10:23:24 -0700

    Make sure I use the right class...

--- app/models/submission_contents/general_submission_content.rb
+++ app/models/submission_contents/general_submission_content.rb
@@ -6,14 +6,14 @@ class GeneralSubmissionContent < SubmissionContent
   class << self
     def create_or_update_with_user_project_slide_content(user, project, slide, content)
       if ProjectSubmission.exists_for_user_project_slide?(user, project, slide)
-        self.class.update_with_user_project_slide_content(user, project, slide, content)
+        self.update_with_user_project_slide_content(user, project, slide, content)
       else
-        FreeResponseSubmissionContent.create_with_user_project_slide_content(user, project, slide, content)
+        self.create_with_user_project_slide_content(user, project, slide, content)
       end
     end
 
     def create_with_user_project_slide_content(user, project, slide, content)
-      content_object = self.class.create(content: content)
+      content_object = self.create(content: content)
       project_submission = ProjectSubmission.create_with_user_project_slide_content_object(user, project, slide, content_object)
       content_object
     end
