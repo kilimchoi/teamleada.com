64f14678345d9743d2ec6967279b9741b739b5db
tristantao <tao.tristan@gmail.com>
2014-09-12 20:32:47 -0700

    rough walk through for pt.1 healthcare

--- db/seeds/projects/023_health_care.rb
+++ db/seeds/projects/023_health_care.rb
@@ -25,39 +25,16 @@ puts "============ Created project: #{project.title}."
 ################################################################################
 
 intro_content_one = [
-  ['text-info', "You've just been hired by the city of San Francisco to use analytics to combat its crime issues."],
-  ['text', "The first task they want to do is to better understand the crime patterns throughout the city."],
-  ['text', "You're tasked with creating visualization(s) that will help the city better interpret the crime data that gets updated daily."],
-  ['text', "You're aren't given restrictions on plot type and you must come up with your own analysis."],
-  ['text', "The crime data can be downloaded below:"],
-  ['link', "https://data.sfgov.org/Public-Safety/SFPD-Incidents-Previous-Three-Months/tmnf-yvry?"],
+  ['text-info', "You have a group of datasets for Healthcare data."],
+  ['text', "Each dataset contains part of the picture."],
+  ['text', "Your job is to aggregate the dataset into a single file"],
 ]
 
 intro_content_two = [
-  ['text-info', "The data contains many features including district, category, and crime types."],
-  ['text', "As mentioned before, the type of visualization is entirely up to you."],
-  ['text-warning', "You might want to consider creating plots that encompass the following:"],
-  ['text', " - Where shouldn't you park your car?"],
-  ['text', " - What are the safest locations in SF? What days/times are especially dangerous?"],
-  ['text', " - Are certain thefts more common in certain areas?"],
-  ['text-success', "Make sure to explain in the summary of your findings how your visualization(s) supports your insights."],
 ]
-
-intro_content_three = [
-  ['text-success', "You are free to complete the project in any language/framework you like."],
-  ['text', "Be sure to explain the framework and any external packages with proper documentation."],
-  ['text', "Remember, having a maintainable & legible codebase leads to good karma :)"],
-  ['next_steps', ""],
-]
-
-quiz = Quiz.create!(
-  quiz_id: "crime_viz_0",
-  answer:"3",
-  project: project,
-)
 
 intro_lesson = Lesson.create!(
-  title: "Introduction",
+  title: "Data Intro",
   project: project,
   lesson_id: 0,
 )
@@ -71,12 +48,6 @@ intro_slide_two = Slide.create!(
   parent: intro_lesson,
   content: intro_content_two,
   slide_id: 1,
-)
-
-intro_slide_thre = Slide.create!(
-  parent: intro_lesson,
-  content: intro_content_three,
-  slide_id: 2,
 )
 
 ################################################################################
@@ -118,16 +89,8 @@ code_submission_context = SubmissionContext.create!(
 ################################################################################
 
 report_submission_content_one = [
-  ['text', "Submit a summary of your analysis (less than 500 words)"],
-  ['text-warning', "The report should contain the question you were answering, along with how you used the data to answer it."],
+  ['text', "Submit a summary of what you did (less than 500 words)"],
   ['user_response', ""],
-]
-
-report_submission_content_two = [
-  ['text', "Include the visualization(s) you created by submitting here: "],
-  ['text', 'Use the same submit button to submit multiple visualizations. Make sure you submit image files! The file type must be a PNG or JPEG.'],
-  ['image_submit', nil],
-  ['next_steps', ""],
 ]
 
 report_submission_lesson = Lesson.create!(
@@ -142,12 +105,6 @@ report_submission_slide = Slide.create!(
   slide_id: 0,
 )
 
-report_submission_slide_two = Slide.create!(
-  content: report_submission_content_two,
-  parent: report_submission_lesson,
-  slide_id: 1,
-)
-
 report_submission_context = SubmissionContext.create!(
   title: "Report Submission",
   description: "User is asked to submit a link to the report.",
@@ -157,46 +114,12 @@ report_submission_context = SubmissionContext.create!(
 )
 
 ################################################################################
-##### Video Submission #########################################################
-################################################################################
-
-
-video_submission_content_one = [
-  ['text', 'You can optionally submit a 1 minute video presenting your insights and analysis. Pretend its to your superior.'],
-  ['text', 'Submit the YouTube link and be sure to make it unlisted!'],
-  ['user_response', ""],
-  ['next_steps', ""],
-]
-
-video_lesson = Lesson.create!(
-  title: "Video Submission (Optional)",
-  project: project,
-  lesson_id: 3,
-)
-
-video_slide = Slide.create!(
-  content: video_submission_content_one,
-  parent: video_lesson,
-  slide_id: 0,
-)
-
-video_submission_context = SubmissionContext.create!(
-  title: "Video Submission",
-  description: "User is asked to submit a 1 minute video presenting their analysis and findings.",
-  slide: video_slide,
-  submission_context_id: 0,
-  submission_type: SubmissionContext::RESPONSE,
-  required: false,
-)
-
-
-################################################################################
 ##### Conclusion ###############################################################
 ################################################################################
 
 conclusion_content_one = [
-  ['text-info', "The challenge was aimed at bringing out your creativity and intuition with data without specific directions."],
-  ['text-success', "It's entirely possible to arrive at some useful conclusion (big or small) without having to create sophisticated predictive models."],
+  ['text-info', "Now that we have the data aggregated, we can work on builing a model."],
+  ['text', "We'll focus on the model in Pt.2"],
   ['finish_project_button', "http://www.surveygizmo.com/s3/1654603/Project-Feedback-Form"],
 ]
 
