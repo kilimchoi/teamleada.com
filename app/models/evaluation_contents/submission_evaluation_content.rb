c7a3238f9ef5aabf10bd8dfb400a27ff406feb6a
Mark Miyashita <negativetwelve@gmail.com>
2014-09-15 02:12:52 -0700

    Viewing evaluations works

--- app/models/evaluation_contents/submission_evaluation_content.rb
+++ app/models/evaluation_contents/submission_evaluation_content.rb
@@ -2,4 +2,8 @@ class SubmissionEvaluationContent < ActiveRecord::Base
   self.abstract_class = true
 
   has_one :submission_evaluation, as: :content_object
+
+  delegate :reviewer, to: :submission_evaluation, allow_nil: true
+  delegate :reviewee, to: :submission_evaluation, allow_nil: true
+  delegate :body,     to: :submission_evaluation, allow_nil: true
 end
