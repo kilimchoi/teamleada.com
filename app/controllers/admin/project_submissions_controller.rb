11ec4d451224cf29fa9aa539f2e301d9853bd316
Mark Miyashita <negativetwelve@gmail.com>
2014-09-15 03:35:59 -0700

    Migrate controller over to project_submissions

--- app/controllers/admin/project_submissions_controller.rb
+++ app/controllers/admin/project_submissions_controller.rb
@@ -1,9 +1,9 @@
-class Admin::CodeSubmissionContentsController < Admin::BaseController
-  load_and_authorize_resource except: [:evaluate, :update_evaluation]
+class Admin::ProjectSubmissionsController < Admin::BaseController
+  load_and_authorize_resource :project_submission
 
   def index
-    if params[:tab] == "completed_projects" || params[:tab] == "by_project"
-      if params[:tab] == "completed_projects"
+    if params[:tab] == "completed-projects" || params[:tab] == "by-project"
+      if params[:tab] == "completed-projects"
         @project_statuses = ProjectStatus.all.select{ |project_status| project_status.completed && project_status.completed_all_submissions? }
       else
         @project_statuses = ProjectStatus.all
@@ -11,7 +11,7 @@ class Admin::CodeSubmissionContentsController < Admin::BaseController
       # TODO: This is inefficient but we need an ActiveRecord relationship to paginate.
       @project_statuses = ProjectStatus.where(id: @project_statuses).group_by_user.paginate(page: params[:page])
     else
-      @code_submission_contents = @code_submission_contents.paginate(page: params[:page])
+      @project_submissions = @project_submissions.paginate(page: params[:page])
     end
   end
 
