627fc2916b4a57db0c8882b83ab48da52e092a19
Mark Miyashita <negativetwelve@gmail.com>
2014-09-16 13:21:35 -0700

    Rename submission_evaluations_controller.rb

--- app/controllers/admin/users/projects/project_submissions/code_submission_evaluations_controller.rb
+++
@@ -1,8 +0,0 @@
-class Admin::Users::Projects::ProjectSubmissions::CodeSubmissionEvaluationsController < Admin::Users::Projects::ProjectSubmissions::BaseController
-  load_and_authorize_resource :code_submission_evaluation
-
-  def index
-    @code_submission_evaluations = @user.evaluations_for_project(@project).where(code_submission: @code_submission)
-  end
-
-end
