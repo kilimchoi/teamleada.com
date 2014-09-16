15f519217e0fcb305f008305549f0b1258a5dcf6
Mark Miyashita <negativetwelve@gmail.com>
2014-09-16 13:28:56 -0700

    Add slides link submission evaluation contents

---
+++ app/models/slides_link_submission_evaluation_content.rb
@@ -0,0 +1,2 @@
+class SlidesLinkSubmissionEvaluationContent < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140916001541) do
+ActiveRecord::Schema.define(version: 20140916202759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -516,6 +516,11 @@ ActiveRecord::Schema.define(version: 20140916001541) do
     t.text     "content"
     t.string   "link_type"
     t.integer  "project_submission_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "slides_link_submission_evaluation_contents", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
