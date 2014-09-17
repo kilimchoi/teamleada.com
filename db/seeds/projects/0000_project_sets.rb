d3b23538bd8c60cb0d3ac7de870b595ec1b93fa3
Mark Miyashita <negativetwelve@gmail.com>
2014-09-17 14:42:01 -0700

    Associate mighty hive parts with the project set

--- db/seeds/projects/0000_project_sets.rb
+++ db/seeds/projects/0000_project_sets.rb
@@ -1,6 +1,6 @@
 
 mightyhive = ProjectSet.create(
   uid: 0,
-  title: "MightyHive",
-  description: "",
+  title: "MightyHive: AdTech Split Testing",
+  description: "A project sponsored by MightyHive, an advertising technology company. You will perform data cleaning, de-duplication, and statistical analysis to determine the effectiveness of their marketing technology.",
 )
