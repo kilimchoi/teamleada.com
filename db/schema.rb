42af531d1a920b950ee3951c65788abd848e9488
Mark Miyashita <negativetwelve@gmail.com>
2014-09-17 15:09:56 -0700

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -135,22 +135,6 @@ ActiveRecord::Schema.define(version: 20140917205032) do
     t.date     "date"
   end
 
-  create_table "delayed_jobs", force: true do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
-
   create_table "employer_applications", force: true do |t|
     t.string   "name"
     t.string   "email"
@@ -406,6 +390,14 @@ ActiveRecord::Schema.define(version: 20140917205032) do
     t.datetime "updated_at"
   end
 
+  create_table "project_sets", id: false, force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "uid",         null: false
+  end
+
   create_table "project_statuses", force: true do |t|
     t.integer  "user_id"
     t.integer  "project_id"
@@ -451,6 +443,7 @@ ActiveRecord::Schema.define(version: 20140917205032) do
     t.string   "cover_photo"
     t.boolean  "has_content_submit",    default: false
     t.boolean  "has_written_submit",    default: false
+    t.integer  "project_set_id"
   end
 
   create_table "publications", force: true do |t|
