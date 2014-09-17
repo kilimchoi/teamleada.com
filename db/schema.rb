bee3971839f9473d3b4b093ef944faaafe6cb75f
Mark Miyashita <negativetwelve@gmail.com>
2014-09-16 21:04:11 -0700

    Add video link submission evaluation content

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140916202759) do
+ActiveRecord::Schema.define(version: 20140917040349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -750,6 +750,11 @@ ActiveRecord::Schema.define(version: 20140916202759) do
     t.text     "content"
     t.string   "link_type"
     t.integer  "project_submission_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "video_link_submission_evaluation_contents", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
