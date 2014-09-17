7c546dc51bff2fbc68c5367726eef2b7e5485260
Mark Miyashita <negativetwelve@gmail.com>
2014-09-17 10:15:37 -0700

    Annotate project sets and evaluation contents

--- spec/models/project_set_spec.rb
+++ spec/models/project_set_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: project_sets
+#
+#  title       :string(255)
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#  uid         :integer          not null, primary key
+#
+
 require 'spec_helper'
 
 describe ProjectSet do
