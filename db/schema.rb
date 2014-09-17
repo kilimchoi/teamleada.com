4e45a90f722182c1e647982cfa9ec39a8b9e8e6d
tristantao <tao.tristan@gmail.com>
2014-09-17 14:33:33 -0700

    schema change for project_set

--- db/schema.rb
+++ db/schema.rb
@@ -390,14 +390,6 @@ ActiveRecord::Schema.define(version: 20140917205032) do
     t.datetime "updated_at"
   end
 
-  create_table "project_sets", id: false, force: true do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "uid",         null: false
-  end
-
   create_table "project_statuses", force: true do |t|
     t.integer  "user_id"
     t.integer  "project_id"
@@ -443,7 +435,6 @@ ActiveRecord::Schema.define(version: 20140917205032) do
     t.string   "cover_photo"
     t.boolean  "has_content_submit",    default: false
     t.boolean  "has_written_submit",    default: false
-    t.integer  "project_set_id"
   end
 
   create_table "publications", force: true do |t|
