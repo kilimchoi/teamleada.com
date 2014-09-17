3d1cb25a8641571435f2d012df9848c7ef34dd41
Brian Wayne Liou <brian.liou91@gmail.com>
2014-09-17 14:39:38 -0700

    changed titles

--- db/seeds/projects/030_MH_V2_P3.rb
+++ db/seeds/projects/030_MH_V2_P3.rb
@@ -8,7 +8,7 @@ main_page_content = [
 
 
 project = Project.create!(
-  title:"MightyHive: Part 3",
+  title:"MightyHive Split Testing: Part 3",
   description: main_page_content,
   short_description: "A project sponsored by Mightyhive, an advertising technology company. You will perform data cleaning, de-duplication, and statistical analysis to determine the effectiveness of their marketing technology.",
   number: 30,
