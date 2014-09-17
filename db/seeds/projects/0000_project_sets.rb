47d6adba9a5e616bcd5e330570495a7dd7ecc083
Mark Miyashita <negativetwelve@gmail.com>
2014-09-17 15:06:27 -0700

    Add statricks to the project set seed file

--- db/seeds/projects/0000_project_sets.rb
+++ db/seeds/projects/0000_project_sets.rb
@@ -1,6 +1,11 @@
-
 mightyhive = ProjectSet.create(
   uid: 0,
   title: "MightyHive: AdTech Split Testing",
   description: "A project sponsored by MightyHive, an advertising technology company. You will perform data cleaning, de-duplication, and statistical analysis to determine the effectiveness of their marketing technology.",
+)
+
+statricks = ProjectSet.create(
+  uid: 1,
+  title: "Statricks: Web Scraping with Python",
+  description: "A project sponsored by Statricks, an e-Commerce analytics company. In this data challenge you will build a web scraper using the Beautiful Soup library to extract listing information. This data challenge is for users with experience in Python and some HTML.",
 )
