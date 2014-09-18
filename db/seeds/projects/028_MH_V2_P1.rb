fd7f23d76f49e82c45e41febf8ad8418f702c447
tristantao <tao.tristan@gmail.com>
2014-09-17 19:21:27 -0700

    mh2 pt1 QA

--- db/seeds/projects/028_MH_V2_P1.rb
+++ db/seeds/projects/028_MH_V2_P1.rb
@@ -1,9 +1,9 @@
 main_page_content = [
-  ['text','MightyHive is an advertising technology company that uses retargeting methods to send ads to users online. One product, “Call Center Remarketing”, uses call center log data to retarget those consumers online which did not make a purchase.'],
+  ['text-info', "MightyHive is an advertising technology company that uses retargeting methods to send ads to users online."],
+  ['text', "One product, “Call Center Remarketing”, uses call center log data to retarget those consumers online which did not make a purchase."],
   ['text', 'You are tasked with determining the effectiveness of one of their advertising campaigns with a vacation rental client “Martin’s Travel Agency”.'],
-  ['text', 'You will be submitting both code and written responses of your analysis and final recommendations.'],
+  ['text-success', 'You will be submitting both code and written responses of your analysis and final recommendations.'],
 ]
-
 
 project = Project.create!(
   title:"MightyHive: AdTech Split Testing (Part 1)",
@@ -18,7 +18,7 @@ project = Project.create!(
   category: Project::CHALLENGE,
   company_overview: "",
   is_new: false,
-  deadline: 3.hours,
+  deadline: 4.hours,
   cover_photo: "mightyhive",
   project_set_id: 0,
 )
@@ -27,11 +27,10 @@ project = Project.create!(
 puts "============ Created project: #{project.title}."
 ################################################################################
 
-
 project_overview_content = [
-  ['text', 'This data challenge is based off of work that was directly done by the analytics team at MightyHive.'],
+  ['text-info', 'This data challenge is based off of work that was directly done by the analytics team at MightyHive.'],
   ['text', 'We will first begin by describing the data you are provided and the context of the problem MightyHive was facing.'],
-  ['text', 'You may complete this challenge in the following languages: R, Python, & MATLAB, but the preferred language is R.'],
+  ['text-danger', 'You may complete this challenge in the following languages: R, Python, & MATLAB, but the preferred language is R.'],
   ['lesson_links', nil]
 ]
 
@@ -50,15 +49,17 @@ project_overview_slide = Slide.create!(
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 data_content_one = [
-  ['text', 'The Abandoned Data Set:'],
+  ['text-warning', 'The Abandoned Data Set:'],
   ['link', 'https://s3.amazonaws.com/leada/mh_data/Abandoned_Data_Seed.csv'],
-  ['text', 'The Reservation Data Set:'],
+  ['text-warning', 'The Reservation Data Set:'],
   ['link', 'https://s3.amazonaws.com/leada/mh_data/Reservation_Data_Seed.csv'],
-  ['text', 'There are two types of observations in the two data sets provided. Abandoned observations are the potential customers who called into the call center but did not make a purchase. Reservation observations are the customers who called into the call center and made a reservation.'],
+  ['text-info', 'There are two types of observations in the two data sets provided.'],
+  ['text', 'Abandoned observations are the potential customers who called into the call center but did not make a purchase.'],
+  ['text', 'Reservation observations are the customers who called into the call center and made a reservation.'],
 ]
 
 data_content_two = [
-  ['text', 'The schema for both of the data sets is provided below:'],
+  ['text-info', 'The schema for both of the data sets is provided below:'],
   ['text', 'Caller_ID - A unique ID given to each incoming phone call to the call center'],
   ['text', 'Session - The Year/Month/Day/Time of each incoming phone call to the call center'],
   ['text', 'Incoming_Phone - Phone number identified using caller indentification'],
@@ -87,9 +88,9 @@ data_content_slide_two = Slide.create!(
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 experiment_content_one = [
-  ['text', 'The analytics team designed an experiment where the Abandoned observations were randomly split into test and control groups with an approximate 50-50 split.'],
+  ['text-info', 'The analytics team designed an experiment where the Abandoned observations were randomly split into test and control groups with an approximate 50-50 split.'],
   ['text', 'The Abandoned observations in the test group were retargeted with advertisements online for a period of 7 days.'],
-  ['text', 'The control group was never show any advertisements.'],
+  ['text', 'The control group was never shown any advertisements.'],
   ['text', 'The Reservation observations include observations from both the test and control groups.'],
   ['next_steps', nil]
 ]
@@ -109,9 +110,11 @@ experiment_slide = Slide.create!(
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 challenge_part_one = [
-  ['text', 'Your overall task is to analyze the data provided and return a conclusion of the effectiveness of the MightyHive Call Center Remarketing product. There are three parts to completing this project.'],
-  ['text', 'Part One Challenge:'],
-  ['text', 'In part one you must determine how many of the observations in the Abandoned data set match with observations in the Reservation data set. Define what constitutes a match with your own assumptions and explain your thought process in your submission as well as the code which implements the matching.'],
+  ['text', 'Your ultimate objective is to analyze the data provided and return a conclusion of the effectiveness of the MightyHive Call Center Remarketing product.'],
+  ['text', 'There are three parts to completing this project.'],
+  ['text-info', 'Part One Challenge:'],
+  ['text-success', 'In part one you must determine how many of the observations in the Abandoned data set match with observations in the Reservation data set.'],
+  ['text', 'Define what constitutes a match with your own assumptions and explain your thought process in your submission as well as the code which implements the matching.'],
   ['lesson_links', nil],
 ]
 
@@ -132,12 +135,14 @@ challenge_slide = Slide.create!(
 
 
 data_matching_content_one = [
-  ['text', 'Explain how you define a match between the Abandoned and Reservation data sets. There is no correct answer, use your best judgement and maximize the number of possible matches.'],
+  ['text', 'Explain how you define a match between the Abandoned and Reservation data sets.'],
+  ['text-warning', 'There is no correct answer, use your best judgement and maximize the number of possible matches.'],
   ['user_response', '#@TODO Include UID'],
 ]
 
 data_matching_content_two = [
-  ['text', 'Submit code which performs the data matching under the assumptions you previously defined. Make sure your code is executable from loading in the data to identifying matches.'],
+  ['text', 'Submit code which performs the data matching under the assumptions you previously defined.'],
+  ['text-warning', 'Make sure your code is executable from loading in the data to identifying matches.'],
   ['text', 'Answer in the quiz below the number of matches you identified'],
   ['quiz', 'match_quiz'],
   ['user_code', '#@TODO Include UID'],
