9a6ffc59482df60ba5f37e666c7df69161cf8c01
Mark Miyashita <negativetwelve@gmail.com>
2014-09-19 11:42:39 -0700

    Redirect to crime vis if you son't have access

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,13 @@
 class LessonsController < ApplicationController
   load_and_authorize_resource :project
-  load_and_authorize_resource :lesson, through: :project
+  load_resource :lesson, through: :project
 
   def show
+    unless can? :show, Lesson
+      flash[:warning] = "You must complete the Crime Visualization project to gain access to more projects!"
+      redirect_to project_path(Project.grants_project_access.first)
+    end
+
     # TODO: Right now we're giving points to users when they 'start' a lesson
     # We should give them the points when they complete the lesson.
     current_user.complete_lesson(@lesson)
