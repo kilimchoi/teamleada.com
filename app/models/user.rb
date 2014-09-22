0b6fb5900cbdd08f2f1c824bfb7f467db556fb5a
tristantao <tao.tristan@gmail.com>
2014-09-22 16:17:51 -0700

    has_started_project uses try()

--- app/models/user.rb
+++ app/models/user.rb
@@ -482,7 +482,8 @@ class User < ActiveRecord::Base
 
   def has_started_project?(project)
     LessonStatus.where(user: self, project: project, completed: true).count +
-    StepStatus.where(user: self, project: project, completed: true).count > 0 && !self.project_statuses.find_by(project: project).start_date.nil?
+    StepStatus.where(user: self, project: project, completed: true).count > 0 &&
+    !self.project_statuses.find_by(project: project).try(:start_date).nil?
   end
 
   def has_finished_project?(project)
