cf5fdbb1327b61fc459ef4cd767e1ad41bf7434f
Mark Miyashita <negativetwelve@gmail.com>
2014-09-23 14:03:35 -0700

    Add industry and info to the first seeded question

--- db/seeds/development/007_interview_questions.rb
+++ db/seeds/development/007_interview_questions.rb
@@ -1,8 +1,10 @@
 
 InterviewQuestion.where(
+  uid: 0,
   title: "Best Restaurant",
   difficulty_level: 9,
   question: "What is the best restaurant in Berkeley",
   posted_at: DateTime.parse("September 23, 2014 12:00:00 PM"),
-  industry: 
+  industry: InterviewQuestion.industries[:data_science],
+  question_type: InterviewQuestion.question_types[:free_response],
 )
