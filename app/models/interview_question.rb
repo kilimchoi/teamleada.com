b00ac209ba62e09ec81e5a3628ebfad1a8e525f7
Mark Miyashita <negativetwelve@gmail.com>
2014-09-23 13:28:48 -0700

    Add relationships to interview questions

--- app/models/interview_question.rb
+++ app/models/interview_question.rb
@@ -1,2 +1,4 @@
 class InterviewQuestion < ActiveRecord::Base
+  has_many :interview_question_submissions
+  has_many :users, through: :interview_question_submissions
 end
--- app/models/interview_question_submission.rb
+++ app/models/interview_question_submission.rb
@@ -1,2 +1,4 @@
 class InterviewQuestionSubmission < ActiveRecord::Base
+  belongs_to :interview_question
+  belongs_to :user
 end
