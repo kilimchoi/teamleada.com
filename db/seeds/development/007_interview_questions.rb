8e2ebe6c0b5caa52494386a4f51ebbf904f32cac
Mark Miyashita <negativetwelve@gmail.com>
2014-09-23 14:14:20 -0700

    Fix the sidebar to the right

--- db/seeds/development/007_interview_questions.rb
+++ db/seeds/development/007_interview_questions.rb
@@ -7,4 +7,4 @@ InterviewQuestion.where(
   posted_at: DateTime.parse("September 23, 2014 12:00:00 PM"),
   industry: InterviewQuestion.industries[:data_science],
   question_type: InterviewQuestion.question_types[:free_response],
-)
+).first_or_create
