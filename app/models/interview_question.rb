4fbd34abd0b7cfb97d88dc601f099ce69db7a5d1
Mark Miyashita <negativetwelve@gmail.com>
2014-09-23 14:00:45 -0700

    Add enums to interview question columns

--- app/models/interview_question.rb
+++ app/models/interview_question.rb
@@ -1,4 +1,7 @@
 class InterviewQuestion < ActiveRecord::Base
+  enum industry: [ :data_science ]
+  enum question_type: [ :free_response, :code, :multiple_choice ]
+
   has_many :interview_question_submissions
   has_many :users, through: :interview_question_submissions
 end
---
+++ db/seeds/development/007_interview_questions.rb
@@ -0,0 +1,8 @@
+
+InterviewQuestion.where(
+  title: "Best Restaurant",
+  difficulty_level: 9,
+  question: "What is the best restaurant in Berkeley",
+  posted_at: DateTime.parse("September 23, 2014 12:00:00 PM"),
+  industry: 
+)
