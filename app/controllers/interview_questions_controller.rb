0311179faa581394dcda9fb4103473161e0bc1d4
Mark Miyashita <negativetwelve@gmail.com>
2014-09-23 14:45:50 -0700

    Load interview questions for every route

--- app/controllers/interview_questions_controller.rb
+++ app/controllers/interview_questions_controller.rb
@@ -1,10 +1,17 @@
 class InterviewQuestionsController < ApplicationController
   load_and_authorize_resource :interview_question
+  before_filter :load_interview_questions
 
   def index
   end
 
   def show
+  end
+
+  private
+
+  def load_interview_questions
+    @interview_questions = InterviewQuestion.all
   end
 
 end
