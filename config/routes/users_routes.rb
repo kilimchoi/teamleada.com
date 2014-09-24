605da6a059a234ba8ba076c4ce85427dbb1d306b
Mark Miyashita <negativetwelve@gmail.com>
2014-09-24 14:07:04 -0700

    Move profile edit routes under users show

--- config/routes/users_routes.rb
+++ config/routes/users_routes.rb
@@ -1,17 +1,17 @@
 TeamLeada::Application.routes.draw do
 
-#  match "profile", to: "users#profile", as: "show_user_profile", via: :get
-  match "profile/edit", to: "users#edit_profile", as: "edit_user_profile", via: :get
-
-  match "profile/edit/cancel", to: "user_profiles#cancel_update", as: "cancel_update_user_profile", via: :post
-  match "profile/edit/about", to: "user_profiles#update_about", as: "update_about_user_profile", via: :post
-  match "profile/edit/job_experience", to: "user_profiles#update_job_experience", as: "update_job_experience_user_profile", via: :post
-  match "profile/edit/job_experience", to: "user_profiles#remove_job_experience", as: "remove_job_experience_user_profile", via: :delete
-  match "profile/edit/enrollment", to: "user_profiles#update_enrollment", as: "update_enrollment_user_profile", via: :post
-
-  match 'settings', to: 'users#edit', as: 'edit_user', via: :get
-
   resources :users, path: "", only: [:show, :update] do
+    member do
+      match "edit", to: "users#edit_profile", as: "edit_user_profile", via: :get
+
+      match "edit/cancel", to: "user_profiles#cancel_update", as: "cancel_update_user_profile", via: :post
+      match "edit/about", to: "user_profiles#update_about", as: "update_about_user_profile", via: :post
+      match "edit/job_experience", to: "user_profiles#update_job_experience", as: "update_job_experience_user_profile", via: :post
+      match "edit/job_experience", to: "user_profiles#remove_job_experience", as: "remove_job_experience_user_profile", via: :delete
+      match "edit/enrollment", to: "user_profiles#update_enrollment", as: "update_enrollment_user_profile", via: :post
+
+      match "settings", to: "users#edit", as: "edit_user", via: :get
+    end
 
     scope module: :users do
       resources :stories, path: "posts", only: [:show, :index]
