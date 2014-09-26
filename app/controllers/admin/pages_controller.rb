d6319b1a65631fc2b916300ea1ed37f30c364d9d
Mark Miyashita <negativetwelve@gmail.com>
2014-09-25 20:50:32 -0700

    Change code submission to project submissions

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -18,7 +18,7 @@ class Admin::PagesController < Admin::BaseController
     @daily_project_impressions = @daily_impressions.filter_category("projects")
     @daily_company_impressions = @daily_impressions.filter_category("companies")
     @daily_new_users = User.where("created_at >= ?", Date.today.to_date)
-    @daily_code_submissions = CodeSubmissionContent.where("created_at >= ?", Date.today.to_date)
+    @daily_project_submissions = ProjectSubmission.where("created_at >= ?", Date.today.to_date)
   end
 
   def active_users_charts
