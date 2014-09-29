87e502e1707b82d3d6eebbcb9b920c38905e6632
tristantao <tao.tristan@gmail.com>
2014-09-28 21:04:31 -0700

    adding more questions

--- db/seeds/interviews/001_interviews.rb
+++ db/seeds/interviews/001_interviews.rb
@@ -10,20 +10,82 @@ InterviewQuestion.create!(
 
 InterviewQuestion.create!(
   uid: 1,
-  title: "Best Boba",
-  difficulty_level: 10,
-  question: "What is the best boba in Berkeley?",
+  title: "Text Mining",
+  difficulty_level: 3,
+  question: "What is an N-Gram in the context of text mining?",
+  posted_at: DateTime.parse("September 23, 2014 12:00:00 PM"),
+  industry: InterviewQuestion.industries[:data_science],
+  question_type: InterviewQuestion.question_types[:free_response],
+)
+
+InterviewQuestion.create!(
+  uid: 2,
+  title: "Basic Stats with Hadoop",
+  difficulty_level: 7,
+  question: "How would you leverage Hadoop to find the median of 1 trillion numbers?",
+  posted_at: DateTime.parse("September 23, 2014 12:00:00 PM"),
+  industry: InterviewQuestion.industries[:data_science],
+  question_type: InterviewQuestion.question_types[:free_response],
+)
+
+InterviewQuestion.create!(
+  uid: 3,
+  title: "PCA",
+  difficulty_level: 7,
+  question: "How does Principle Component Analysis, and how is it used?",
+  posted_at: DateTime.parse("September 23, 2014 12:00:00 PM"),
+  industry: InterviewQuestion.industries[:data_science],
+  question_type: InterviewQuestion.question_types[:free_response],
+)
+
+InterviewQuestion.create!(
+  uid: 4,
+  title: "Brain Teaser",
+  difficulty_level: 9,
+  question: "Imagine you're an ant starting at a vertex on a cube. If you begin embarking on a random walk, what is the expected number of edges that you will taaverse, before you've visited all the vertices?",
+  posted_at: DateTime.parse("September 23, 2014 12:00:00 PM"),
+  industry: InterviewQuestion.industries[:data_science],
+  question_type: InterviewQuestion.question_types[:free_response],
+)
+
+InterviewQuestion.create!(
+  uid: 5,
+  title: "Classmates",
+  difficulty_level: 8,
+  question: "Say you're in a classroom with 50 people. Which of the following is the closest to the probability that there is another person with the same birthday as you?",
   posted_at: DateTime.parse("September 23, 2014 12:00:00 PM"),
   industry: InterviewQuestion.industries[:data_science],
   question_type: InterviewQuestion.question_types[:multiple_choice],
 )
 
 InterviewQuestion.create!(
-  uid: 2,
-  title: "Best Band",
-  difficulty_level: 5,
-  question: "What is the best band ever?",
+  uid: 6,
+  title: "Balls",
+  difficulty_level: 7,
+  question: "There are 2 bags. One has 2 blue balls, the other has 1 blue, 1 yellow. You randomly pick a bag, and followed by a ball from the bag. The ball is blue. What is the probability that the 2nd ball from the bag is blue?",
   posted_at: DateTime.parse("September 23, 2014 12:00:00 PM"),
   industry: InterviewQuestion.industries[:data_science],
-  question_type: InterviewQuestion.question_types[:code],
+  question_type: InterviewQuestion.question_types[:free_response],
 )
+
+InterviewQuestion.create!(
+  uid: 7,
+  title: "Trade-offs",
+  difficulty_level: 7,
+  question: "Explain the Bias-Variance tradeoff, and what that means for Data Scientists.",
+  posted_at: DateTime.parse("September 23, 2014 12:00:00 PM"),
+  industry: InterviewQuestion.industries[:data_science],
+  question_type: InterviewQuestion.question_types[:free_response],
+)
+
+InterviewQuestion.create!(
+  uid: 8,
+  title: "Assumptions",
+  difficulty_level: 7,
+  question: "Explain the assumptions made under the Standard Linear Regression models.",
+  posted_at: DateTime.parse("September 23, 2014 12:00:00 PM"),
+  industry: InterviewQuestion.industries[:data_science],
+  question_type: InterviewQuestion.question_types[:free_response],
+)
+
+  #question_type: InterviewQuestion.question_types[:multiple_choice],
