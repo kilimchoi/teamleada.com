cbb8474278a123acb49d13260998ab82a7e3c1e1
Mark Miyashita <negativetwelve@gmail.com>
2014-09-29 12:24:59 -0700

    Add views for index viewing interview_question_submissions

--- app/controllers/admin/interview_question_submissions_controller.rb
+++ app/controllers/admin/interview_question_submissions_controller.rb
@@ -2,6 +2,7 @@ class Admin::InterviewQuestionSubmissionsController < Admin::BaseController
   load_and_authorize_resource
 
   def index
+    @interview_question_submissions = @interview_question_submissions.paginate(page: params[:page])
   end
 
   def show
