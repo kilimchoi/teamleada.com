bf4a1556c445f1b35bafcfc5290907d325281e15
Mark Miyashita <negativetwelve@gmail.com>
2014-09-29 10:09:30 -0700

    Add tag model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925002842) do
+ActiveRecord::Schema.define(version: 20140929165122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -655,6 +655,13 @@ ActiveRecord::Schema.define(version: 20140925002842) do
     t.string   "subscriber_type"
     t.integer  "subscribable_id"
     t.string   "subscribable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.integer  "taggings_count"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
