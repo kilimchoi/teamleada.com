88c09a7736823f5b3f557645627b6f0f6db5b095
Mark Miyashita <negativetwelve@gmail.com>
2014-09-28 17:04:27 -0700

    Add submit route and combine form and title

--- config/routes/interview_question_routes.rb
+++ config/routes/interview_question_routes.rb
@@ -1,7 +1,9 @@
 TeamLeada::Application.routes.draw do
 
   resources :interview_questions, path: "interview-questions", only: [:index, :show] do
-
+    member do
+      match "submit", to: "interview_questions#submit", via: :post
+    end
   end
 
 end
