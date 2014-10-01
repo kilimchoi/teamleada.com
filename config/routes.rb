0c4584addd45fa19e84ce27d67d1e71fcc0ab7e9
Mark Miyashita <negativetwelve@gmail.com>
2014-10-01 11:08:40 -0700

    Always use double quotes

--- config/routes.rb
+++ config/routes.rb
@@ -5,63 +5,63 @@ class ActionDispatch::Routing::Mapper
 end
 
 TeamLeada::Application.routes.draw do
-  mount Peek::Railtie => '/peek'
+  mount Peek::Railtie => "/peek"
 
-  root to: 'pages#home'
+  root to: "pages#home"
 
-  match 'about', to: 'pages#about', via: :get
-  #match 'tutoring', to: 'pages#tutoring', via: :get
-  #match 'tutoring/purchase', to: 'pages#tutoring_purchase', as: 'tutoring_purchase', via: :post
+  match "about", to: "pages#about", via: :get
+  #match "tutoring", to: "pages#tutoring", via: :get
+  #match "tutoring/purchase", to: "pages#tutoring_purchase", as: "tutoring_purchase", via: :post
 
-  match 'invite', to: 'invites#index', as: 'invites', via: :get
-  match 'invite', to: 'invites#create', as: 'invite_user', via: :post
+  match "invite", to: "invites#index", as: "invites", via: :get
+  match "invite", to: "invites#create", as: "invite_user", via: :post
 
-  match 'learn', to: 'pages#student', as: :learn, via: :get
-  match 'learn', to: 'users#auth_code', as: :auth_code, via: :post
-  match 'ask-peter', to: 'pages#question_answer', as: 'question_answer', via: :get
-  match 'ask-peter/guidelines', to: 'pages#question_guidelines', as: 'question_guidelines', via: :get
+  match "learn", to: "pages#student", as: :learn, via: :get
+  match "learn", to: "users#auth_code", as: :auth_code, via: :post
+  match "ask-peter", to: "pages#question_answer", as: "question_answer", via: :get
+  match "ask-peter/guidelines", to: "pages#question_guidelines", as: "question_guidelines", via: :get
 
-  match 'handbook', to: 'pages#handbook', as: :handbook, via: :get
+  match "handbook", to: "pages#handbook", as: :handbook, via: :get
   match "how-it-works", to: "pages#how_it_works", as: :how_it_works, via: :get
 
-  devise_for :users, path: '',
-                     path_names: { sign_in: 'login', sign_up: 'sign-up', sign_out: 'logout'},
+  devise_for :users, path: "",
+                     path_names: { sign_in: "login", sign_up: "sign-up", sign_out: "logout"},
                      controllers: {
-                       registrations: 'registrations',
-                       sessions: 'sessions',
-                       confirmations: 'confirmations',
-                       passwords: 'passwords',
-                       omniauth_callbacks: 'omniauth_callbacks'
+                       registrations: "registrations",
+                       sessions: "sessions",
+                       confirmations: "confirmations",
+                       passwords: "passwords",
+                       omniauth_callbacks: "omniauth_callbacks"
                      }
 
   devise_scope :user do
-    match 'confirm', to: "confirmations#confirm", as: :confirm, via: :get
-    match 'linkedin-confirm', to: "confirmations#linkedin_confirm", as: :linkedin_confirm, via: :patch
-    match 'linkedin-confirm', to: "confirmations#show_linkedin_confirm", as: :show_linkedin_confirm, via: :get
+    match "confirm", to: "confirmations#confirm", as: :confirm, via: :get
+    match "linkedin-confirm", to: "confirmations#linkedin_confirm", as: :linkedin_confirm, via: :patch
+    match "linkedin-confirm", to: "confirmations#show_linkedin_confirm", as: :show_linkedin_confirm, via: :get
   end
 
   # Debug Bar
-  match 'change-role', to: "users#change_role", as: :user_change_role, via: :get
+  match "change-role", to: "users#change_role", as: :user_change_role, via: :get
 
   # Messages
-  match 'messages', to: 'messages#create', as: 'messages', via: :post
-  match 'conversations', to: 'conversations#create', as: 'create_conversation', via: :post
+  match "messages", to: "messages#create", as: "messages", via: :post
+  match "conversations", to: "conversations#create", as: "create_conversation", via: :post
   resources :conversations, path: :messages, only: [:show, :index, :new] do
     get :autocomplete_user_name, on: :collection
   end
 
   resources :interested_users, only: [:create]
 
-  resources :questions, path: 'ask-peter', only: [:show, :index, :new, :create] do
+  resources :questions, path: "ask-peter", only: [:show, :index, :new, :create] do
     member do
-      match 'up-vote', to: 'questions#up_vote', as: 'up_vote', via: :post
+      match "up-vote", to: "questions#up_vote", as: "up_vote", via: :post
     end
   end
 
-  match 'employer', to: 'employer_applications#new', as: 'new_employer', via: :get
-  resources :employer_applications, path: 'employer', as: 'employer', only: [:create]
+  match "employer", to: "employer_applications#new", as: "new_employer", via: :get
+  resources :employer_applications, path: "employer", as: "employer", only: [:create]
 
-  get '/projects', to: redirect('/companies')
+  get "/projects", to: redirect("/companies")
 
   draw :interview_question_routes
   draw :project_routes
@@ -72,6 +72,6 @@ TeamLeada::Application.routes.draw do
   draw :users_routes
 
   # 404 page routes
-  match '*path', to: 'pages#error', via: :get
+  match "*path", to: "pages#error", via: :get
 end
 
