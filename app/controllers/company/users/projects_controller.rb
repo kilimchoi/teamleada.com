399abe5a3d3b936d0b42d6605b4aab25dd2458d2
Mark Miyashita <negativetwelve@gmail.com>
2014-10-01 13:41:10 -0700

    Load user submissions on the project page

--- app/controllers/company/users/projects_controller.rb
+++ app/controllers/company/users/projects_controller.rb
@@ -1,10 +1,20 @@
 class Company::Users::ProjectsController < Company::Users::BaseController
-  load_and_authorize_resource
+  load_and_authorize_resource :project
+  # TODO(mark): Why isn't load_and_authorize_resource working...
+  before_filter :load_user
+
+  layout "company/users"
 
   def index
   end
 
   def show
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by(username: params[:browse_user_id])
   end
 
 end
